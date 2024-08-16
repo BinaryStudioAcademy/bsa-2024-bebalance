c43de9d98c2246da8c6fb1b77dca42c7d59a76ac
Dmitrij Revenets <revenets@gmail.com>
2024-08-16 18:42:46 +0300

    feat: added custom entry point bb-2

---
+++ apps/mobile/index.jsx
@@ -0,0 +1,10 @@
+import { registerRootComponent } from 'expo';
+import { ExpoRoot } from 'expo-router';
+
+export function App() {
+  const ctx = require.context('./src/app');
+
+  return <ExpoRoot context={ctx} />;
+}
+
+registerRootComponent(App);
