c8cc1c638d15de868c3fd5964bd429e3ea3c1f90
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-08-17 21:26:01 +0300

    feat: init global-styles bb-17

---
+++ apps/mobile/src/styles/global-styles/global-styles.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+import { FLEX } from "~/styles/global-styles/flex";
+
+const globalStyles = StyleSheet.create({
+	...FLEX,
+});
+
+export { globalStyles };
