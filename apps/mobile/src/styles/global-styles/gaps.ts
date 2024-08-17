131b395b251588ed92c1c486f5ccb15b2ad59ab3
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-08-17 22:10:20 +0300

    feat: add gaps bb-17

---
+++ apps/mobile/src/styles/global-styles/gaps.ts
@@ -0,0 +1,11 @@
+import { SPACINGS } from "~/styles/global-styles/spacings";
+
+const GAPS = {
+	gapXs: { gap: SPACINGS.xs },
+	gapSm: { gap: SPACINGS.md },
+	gapLg: { gap: SPACINGS.lg },
+	gapXl: { gap: SPACINGS.xl },
+	gapXXl: { gap: SPACINGS.xxl },
+};
+
+export { GAPS };
