107a614cf911ead37f00380e442ed0bfae14c7f8
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-08-17 20:57:11 +0300

    feat: add flex bb-17

---
+++ apps/mobile/src/styles/global-styles/flex.ts
@@ -0,0 +1,34 @@
+const FLEX = {
+	flex1: {
+		flex: 1,
+	},
+	flexDirectionRow: {
+		flexDirection: 'row',
+	},
+	flexDirectionColumn: {
+		flexDirection: 'column',
+	},
+	alignItemsCenter: {
+		alignItems: 'center',
+	},
+	alignItemsStart: {
+		alignItems: 'flex-start',
+	},
+	alignItemsEnd: {
+		alignItems: 'flex-end',
+	},
+	justifyContentCenter: {
+		justifyContent: 'center',
+	},
+	justifyContentStart: {
+		justifyContent: 'flex-start',
+	},
+	justifyContentEnd: {
+		justifyContent: 'flex-end',
+	},
+	justifyContentSpaceBetween: {
+		justifyContent: 'space-between',
+	},
+};
+
+export { FLEX };
