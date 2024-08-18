c7e31432afc0c16c1afdc7d9da5074fd20d10c8e
luiqor <vohnenna@gmail.com>
2024-08-18 19:01:36 +0300

    feat: add name as required for sign up validation bb-8

--- packages/shared/src/modules/users/libs/enums/user-validation-message.enum.ts
+++ packages/shared/src/modules/users/libs/enums/user-validation-message.enum.ts
@@ -2,6 +2,7 @@ const UserValidationMessage = {
 	EMAIL_REQUIRE: "Email is required",
 	EMAIL_TAKEN: "Email is taken",
 	EMAIL_WRONG: "Email is wrong",
+	NAME_REQUIRE: "Name is required",
 } as const;
 
 export { UserValidationMessage };
