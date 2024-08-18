a5c24416e6e46d09dd02c1d813db0b99df7fa423
Tetiana Brezhynska <57232185+Shcracoziabra@users.noreply.github.com>
2024-08-18 16:12:42 +0200

    feat: colors enum bb-23 (#36)

---
+++ apps/mobile/src/libs/enums/app/base-color.enum.ts
@@ -0,0 +1,14 @@
+const BaseColor = {
+	BG_BLUE: "rgba(118, 172, 236, 1)",
+	BG_LIGHT: "rgba(246, 245, 250, 1)",
+	BG_WHITE: "rgba(255, 255, 255, 1)",
+	BLACK: "rgba(0, 0, 0, 1)",
+	DARK_GRAY: "rgba(124, 124, 124, 1)",
+	GRAY: "rgba(176, 176, 178, 1)",
+	GREEN: "rgba(194, 234, 205, 1)",
+	LIGHT_GRAY: "rgba(224, 224, 224, 1)",
+	LIGHT_GREEN: "rgba(240, 255, 244, 1)",
+	RED: "rgba(255, 41, 40, 1)",
+} as const;
+
+export { BaseColor };
