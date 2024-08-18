d03ed994144f13f347c741ed0e0d2da9ddaf62f0
Farid Shabanov <farid.shabanov@binary-studio.com>
2024-08-18 18:59:02 +0400

    build: update lint-staged commands bb-0

--- apps/mobile/eslint.config.mjs
+++ apps/mobile/eslint.config.mjs
@@ -21,8 +21,27 @@ const mainConfig = {
 const overridesConfigs = [
 	{
 		rules: {
+			"@typescript-eslint/no-unnecessary-type-parameters": ["off"],
 			"import/extensions": ["off"],
-			"@typescript-eslint/no-unnecessary-type-parameters": ["off"]
+		},
+	},
+	{
+		files: ["babel.config.js", "metro.config.js", "lint-staged.config.mjs"],
+		rules: {
+			"@typescript-eslint/no-require-imports": ["off"],
+			"unicorn/prefer-module": ["off"],
+		},
+	},
+	{
+		files: ["eslint.config.mjs", "lint-staged.config.mjs"],
+		rules: {
+			"import/no-default-export": ["off"],
+		},
+	},
+	{
+		files: ["babel.config.js"],
+		rules: {
+			"unicorn/prefer-node-protocol": ["off"],
 		},
 	},
 ];
