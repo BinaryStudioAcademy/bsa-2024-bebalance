8d68c7871000216c5dc3a4f00d64577819106055
luiqor <vohnenna@gmail.com>
2024-08-18 18:52:26 +0300

    feat: add relation mapping for user details bb-8

--- apps/backend/src/modules/users/user.model.ts
+++ apps/backend/src/modules/users/user.model.ts
@@ -1,14 +1,31 @@
+import { Model } from "objection";
+
 import {
 	AbstractModel,
 	DatabaseTableName,
 } from "~/libs/modules/database/database.js";
 
+import { UserDetailsModel } from "./user-details.model.js";
+
 class UserModel extends AbstractModel {
+	static relationMappings = {
+		userDetails: {
+			join: {
+				from: `${DatabaseTableName.USERS}.id`,
+				to: `${DatabaseTableName.USER_DETAILS}.userId`,
+			},
+			modelClass: UserDetailsModel,
+			relation: Model.HasOneRelation,
+		},
+	};
+
 	public email!: string;
 
 	public passwordHash!: string;
 
 	public passwordSalt!: string;
+
+	public userDetails!: UserDetailsModel;
 
 	public static override get tableName(): string {
 		return DatabaseTableName.USERS;
