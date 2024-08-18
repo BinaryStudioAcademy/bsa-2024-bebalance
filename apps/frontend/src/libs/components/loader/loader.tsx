77851c047f76a5d9559130cc4ae90b2cb03c07ba
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-08-18 21:20:58 +0300

    feat: add loader text bb-15

--- apps/frontend/src/libs/components/loader/loader.tsx
+++ apps/frontend/src/libs/components/loader/loader.tsx
@@ -3,7 +3,12 @@ import React from "react";
 import styles from "./styles.module.css";
 
 const Loader: React.FC = () => {
-	return <div className={styles["loader"]}></div>;
+	return (
+		<div className={styles["loaderContainer"]}>
+			<div className={styles["loader"]}></div>
+			<p className={styles["statusText"]}>Loading...</p>
+		</div>
+	);
 };
 
 export { Loader };
