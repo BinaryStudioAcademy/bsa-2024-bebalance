78d6259dd75675033c1129996044f0f8b36039c5
KotenkoNata <kotenkonata8@gmail.com>
2024-08-19 10:46:21 +0200

    fix: perfectionist/sort-named-imports bb-18

--- apps/mobile/src/libs/components/text/text.tsx
+++ apps/mobile/src/libs/components/text/text.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
-import { StyleProp, Text as RNText, TextStyle } from "react-native";
+import { Text as RNText, StyleProp, TextStyle } from "react-native";
 
 import {
 	BaseColor,
