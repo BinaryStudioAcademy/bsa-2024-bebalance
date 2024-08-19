8a9773df306d4d20b7470053e794b05c77e6b8f2
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-08-19 09:13:11 +0300

    refactor: rename spacing variables in GAPS for consistency bb-17

--- apps/mobile/src/libs/styles/global-styles/gaps.ts
+++ apps/mobile/src/libs/styles/global-styles/gaps.ts
@@ -1,14 +1,14 @@
-import { SPACINGS } from "~/libs/styles/global-styles/spacings";
+import { SPACING } from "~/libs/styles/global-styles/spacing";
 
 const GAPS = {
-	gapLg: { gap: SPACINGS.lg },
-	gapMd: { gap: SPACINGS.md },
-	gapSm: { gap: SPACINGS.sm },
-	gapXl: { gap: SPACINGS.xl },
-	gapXs: { gap: SPACINGS.xs },
-	gapXxl: { gap: SPACINGS.xxl },
-	gapXxs: { gap: SPACINGS.xxs },
-	gapXxxs: { gap: SPACINGS.xxxs },
+	gap2: { gap: SPACING.xxxs },
+	gap4: { gap: SPACING.xxs },
+	gap8: { gap: SPACING.xs },
+	gap12: { gap: SPACING.sm },
+	gap16: { gap: SPACING.md },
+	gap24: { gap: SPACING.lg },
+	gap32: { gap: SPACING.xl },
+	gap48: { gap: SPACING.xxl },
 };
 
 export { GAPS };
