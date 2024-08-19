52a0f734f6e85125b4db6129afdafe5e9fae57ba
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-08-19 14:23:54 +0300

    feat: add loader bb-15 (#45)
    * feat: add loader component bb-15
    
    * feat: add suspense to index.tsx bb-15
    
    * fix: icons background position bb-15
    
    * fix: icon background-position bb-15
    
    * fix: loader component bb-15
    
    * fix: delete unnecessary icons bb-15
    
    * feat: add loader text bb-15
    
    * feat: add gradient to loader bb-15
    
    * refactor: remove unnecessary bb-15
    
    * refactor: remove unnecessary import bb-15

---
+++ apps/frontend/src/libs/components/loader/loader.tsx
@@ -0,0 +1,12 @@
+import styles from "./styles.module.css";
+
+const Loader: React.FC = () => {
+	return (
+		<div className={styles["loaderContainer"]}>
+			<div className={styles["loader"]} />
+			<p className={styles["statusText"]}>Loading...</p>
+		</div>
+	);
+};
+
+export { Loader };
