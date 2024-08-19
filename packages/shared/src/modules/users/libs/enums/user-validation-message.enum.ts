83b31eb785a91c2924439ab595dba1b279da46cf
JD-Rosales <rosales.jake1999@gmail.com>
2024-08-19 10:05:01 +0800

    fix: resolve template literal error in user validation bb-7

--- packages/shared/src/modules/users/libs/enums/user-validation-message.enum.ts
+++ packages/shared/src/modules/users/libs/enums/user-validation-message.enum.ts
@@ -3,8 +3,8 @@ import { UserValidationRule } from "./user-validation-rule.enum.js";
 const UserValidationMessage = {
 	EMAIL_REQUIRE: "Email is required",
 	EMAIL_WRONG: "Email is wrong",
-	PASSWORD_MAX_LENGTH: `Password must be at most ${UserValidationRule.PASSWORD_MAX_LENGTH} characters long`,
-	PASSWORD_MIN_LENGTH: `Password must be at least ${UserValidationRule.PASSWORD_MIN_LENGTH} characters long`,
+	PASSWORD_MAX_LENGTH: `Password must be at most ${UserValidationRule.PASSWORD_MAX_LENGTH.toString()} characters long`,
+	PASSWORD_MIN_LENGTH: `Password must be at least ${UserValidationRule.PASSWORD_MIN_LENGTH.toString()} characters long`,
 } as const;
 
 export { UserValidationMessage };
