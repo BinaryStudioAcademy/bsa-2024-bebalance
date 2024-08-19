e0f7c653821f5ab2ded02eb854d41cd4b0e19800
ihorLysak <lysak.ihor4221@gmail.com>
2024-08-19 17:25:12 +0300

    refactor: changed path to config in token.ts bb-10

--- apps/backend/src/libs/modules/token/token.ts
+++ apps/backend/src/libs/modules/token/token.ts
@@ -1,4 +1,5 @@
-import { config } from "../config/config.js";
+import { config } from "~/libs/modules/config/config.js";
+
 import { BaseToken } from "./base-token.module.js";
 
 const token = new BaseToken({
