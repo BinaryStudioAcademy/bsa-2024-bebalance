4c8b9879a6d0330461d75070098e7381b9edde10
luiqor <vohnenna@gmail.com>
2024-08-19 09:29:13 +0300

    refactor: use constants in add_user_details_table migration for user table and id column bb-8

--- apps/backend/src/db/migrations/20240818081459_add_user_details_table.ts
+++ apps/backend/src/db/migrations/20240818081459_add_user_details_table.ts
@@ -1,6 +1,6 @@
 import type { Knex } from "knex";
 
-const TABLE_NAME = "user_details";
+const TableName = { USER_DETAILS: "user_details", USERS: "users" } as const;
 
 const ColumnName = {
 	CREATED_AT: "created_at",
@@ -11,12 +11,12 @@ const ColumnName = {
 } as const;
 
 function up(knex: Knex): Promise<void> {
-	return knex.schema.createTable(TABLE_NAME, (table) => {
+	return knex.schema.createTable(TableName.USER_DETAILS, (table) => {
 		table.increments(ColumnName.ID).primary();
 		table
 			.integer(ColumnName.USER_ID)
-			.references("id")
-			.inTable("users")
+			.references(ColumnName.ID)
+			.inTable(TableName.USERS)
 			.notNullable();
 		table.string(ColumnName.NAME).notNullable();
 		table
@@ -31,7 +31,7 @@ function up(knex: Knex): Promise<void> {
 }
 
 function down(knex: Knex): Promise<void> {
-	return knex.schema.dropTableIfExists(TABLE_NAME);
+	return knex.schema.dropTableIfExists(TableName.USER_DETAILS);
 }
 
 export { down, up };
