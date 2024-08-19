ce9ce0dc784220cf8467654db597c62931cc8687
Tetiana Brezhynska <brezhynska9012@gmail.com>
2024-08-19 15:01:26 +0200

    feat: changed styles to present filled and outlined btns bb-26

--- apps/mobile/src/libs/components/button/styles.ts
+++ apps/mobile/src/libs/components/button/styles.ts
@@ -5,11 +5,27 @@ import { BaseColor } from "~/libs/enums/enums";
 const styles = StyleSheet.create({
 	btn: {
 		alignItems: "center",
-		backgroundColor: BaseColor.BLACK,
 		padding: 10,
 	},
+	filledBtnLabel: {
+		color: BaseColor.BG_WHITE,
+	},
 	label: {
-		color: "white",
+		fontFamily: "Nunito",
+		fontSize: 16,
+		fontWeight: 700,
+		textTransform: "uppercase",
+	},
+	outlined: {
+		borderWidth: 1,
+	},
+	outlinedInner: {
+		backgroundColor: BaseColor.BG_WHITE,
+		height: "100%",
+		width: "100%",
+	},
+	wrapper: {
+		height: 42,
 	},
 });
 
