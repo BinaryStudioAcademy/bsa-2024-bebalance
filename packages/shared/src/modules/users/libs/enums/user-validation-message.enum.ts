0ff6ad5290d83c1bd8427b50da5e7b2c752b9805
Andrii Levin <andriilevin@gmail.com>
2024-08-19 17:55:27 +0100

    refactor: replace hardcoded password length with constant in UserValidationMessage bb-6

--- packages/shared/src/modules/users/libs/enums/user-validation-message.enum.ts
+++ packages/shared/src/modules/users/libs/enums/user-validation-message.enum.ts
@@ -1,8 +1,10 @@
+import { UserValidationRule } from "./user-validation-rule.enum.js";
+
 const UserValidationMessage = {
 	EMAIL_REQUIRE: "Email is required",
 	EMAIL_WRONG: "Email is wrong",
 	PASSWORD_REQUIRE: "Password is not allowed to be empty",
-	PASSWORD_SHORT: "Password length must be at least 8 characters long",
+	PASSWORD_SHORT: `Password length must be at least ${String(UserValidationRule.PASSWORD_MINIMUM_LENGTH)} characters long`,
 } as const;
 
 export { UserValidationMessage };
