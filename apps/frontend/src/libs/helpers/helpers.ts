a93a329477e1975769b60e79817947686427dcfc
JD-Rosales <rosales.jake1999@gmail.com>
2024-08-20 16:27:27 +0800

    chore: update getValidClassNames helper bb-7

--- apps/frontend/src/libs/helpers/helpers.ts
+++ apps/frontend/src/libs/helpers/helpers.ts
@@ -1,3 +1,3 @@
-export { getValidClassNames } from "./get-valid-class-name.js";
+export { getValidClassNames } from "./get-valid-class-names/get-valid-class-names.js";
 
 export { configureString } from "shared";
--- apps/frontend/src/pages/auth/components/sign-in-form/libs/icons/icons.ts
+++
@@ -1,2 +0,0 @@
-export { RippleEffectBg } from "./ripple-effect-bg.js";
-export { RippleEffectBg2 } from "./ripple-effect-bg2.js";
