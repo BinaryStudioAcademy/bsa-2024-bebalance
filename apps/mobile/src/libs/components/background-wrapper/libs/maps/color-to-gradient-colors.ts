979b4e6037adf92a93c4481ac311dbc6712c3b52
iqbalalayubbi <iqbal.is415@gmail.com>
2024-08-21 20:32:33 +0700

    fix: handle conflict in app.tsx bb-9

---
+++ apps/mobile/src/libs/components/background-wrapper/libs/maps/color-to-gradient-colors.ts
@@ -0,0 +1,9 @@
+import { GradientColor } from "~/libs/enums/enums";
+
+const colorToGradientColors = {
+	blue: [...GradientColor.BLUE],
+	green: [...GradientColor.LIME],
+	pink: [...GradientColor.ROSE],
+};
+
+export { colorToGradientColors };
