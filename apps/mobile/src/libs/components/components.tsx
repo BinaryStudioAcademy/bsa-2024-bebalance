c8e3c026eb85e4ce925eef1dd09cb93968b73f57
Tetiana Brezhynska <brezhynska9012@gmail.com>
2024-08-21 16:22:25 +0200

    style: fixed code style bb-26

--- apps/mobile/src/libs/components/components.tsx
+++ apps/mobile/src/libs/components/components.tsx
@@ -6,5 +6,5 @@ export { Link } from "./link/link";
 export { LoaderWrapper } from "./loader-wrapper/loader-wrapper";
 export { ScreenWrapper } from "./screen-wrapper/screen-wrapper";
 export { Text } from "./text/text";
-export { ActivityIndicator, Pressable, Image, View } from "react-native";
+export { ActivityIndicator, Image, Pressable, View } from "react-native";
 export { LinearGradient } from "react-native-linear-gradient";
