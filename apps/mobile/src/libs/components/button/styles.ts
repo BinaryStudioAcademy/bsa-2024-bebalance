b65ea7560642bd34b94ea4e26e3718da44d13a1d
Tetiana Brezhynska <brezhynska9012@gmail.com>
2024-08-21 15:48:51 +0200

    feat: removed unused styles bb-26

--- apps/mobile/src/libs/components/button/styles.ts
+++ apps/mobile/src/libs/components/button/styles.ts
@@ -6,23 +6,8 @@ const styles = StyleSheet.create({
 	bgWhite: {
 		backgroundColor: BaseColor.BG_WHITE,
 	},
-	colorBlack: {
-		color: BaseColor.BLACK,
-	},
-	colorWhite: {
-		color: BaseColor.BG_WHITE,
-	},
-	p1: {
-		padding: 1,
-	},
 	rounded: {
 		borderRadius: 35,
-	},
-	text: {
-		fontFamily: "Nunito",
-		fontSize: 16,
-		fontWeight: 700,
-		textTransform: "uppercase",
 	},
 	wrapper: {
 		height: 42,
