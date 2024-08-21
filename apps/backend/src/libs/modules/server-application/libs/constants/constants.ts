49847ecd407ab35db0dba103e3a3b5c977b72b58
KeidsID <keidscode@gmail.com>
2024-08-22 00:38:00 +0800

    fix(backend): fix missing white routes bb-11

--- apps/backend/src/libs/modules/server-application/libs/constants/constants.ts
+++ apps/backend/src/libs/modules/server-application/libs/constants/constants.ts
@@ -1,9 +1,10 @@
-import { AuthApiPath } from "~/modules/auth/libs/enums/enums.js";
+import { AuthApiPath } from "~/modules/auth/auth.js";
 
 import { APIPath } from "../enums/enums.js";
 
 const WHITE_ROUTES: string[] = [
 	APIPath.AUTH,
+	`${APIPath.AUTH}${AuthApiPath.SIGN_IN}`,
 	`${APIPath.AUTH}${AuthApiPath.SIGN_UP}`,
 ];
 
