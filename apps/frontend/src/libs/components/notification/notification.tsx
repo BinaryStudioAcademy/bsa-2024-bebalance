442500105802ba497c02978fd1e9af88eb6ae53d
Marharyta Rozghon <72861258+marharita08@users.noreply.github.com>
2024-08-21 16:10:43 +0300

    feat: error handling bb-16 (#57)
    * feat: add toast container bb-16
    
    * feat: add handle error middleware bb-16
    
    * fix: update handle error midleware add type assertion to error bb-16
    
    * fix: add toast module bb-16
    
    * fix: add toast notification module bb-16
    
    * fix: move and rename notification component bb-16
    
    * fix: rename module bb-16
    
    * fix: add middleware error type bb-16
    
    * fix: add default messages for notification enum bb-16
    
    * fix(frontend): replace default message enum with default error message constant bb-16

---
+++ apps/frontend/src/libs/components/notification/notification.tsx
@@ -0,0 +1,8 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Notification = () => {
+	return <ToastContainer />;
+};
+
+export { Notification };
