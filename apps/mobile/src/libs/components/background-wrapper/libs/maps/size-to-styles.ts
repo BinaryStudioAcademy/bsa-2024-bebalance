28727822bde894fdf3c87c020a391fee59b5f889
Tetiana Brezhynska <brezhynska9012@gmail.com>
2024-08-22 23:06:37 +0200

    feat(mobile): added xs size bb-21

--- apps/mobile/src/libs/components/background-wrapper/libs/maps/size-to-styles.ts
+++ apps/mobile/src/libs/components/background-wrapper/libs/maps/size-to-styles.ts
@@ -4,6 +4,7 @@ const sizeToStyles = {
 	lg: { borderRadius: 160, height: 160, width: 160 } satisfies ViewStyle,
 	md: { borderRadius: 100, height: 100, width: 100 } satisfies ViewStyle,
 	sm: { borderRadius: 86, height: 86, width: 86 } satisfies ViewStyle,
+	xs: { borderRadius: 34, height: 34, width: 34 } satisfies ViewStyle,
 };
 
 export { sizeToStyles };
