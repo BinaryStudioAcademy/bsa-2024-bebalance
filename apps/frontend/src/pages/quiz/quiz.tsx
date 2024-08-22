ac2244d4c5ad9976fcb45153f518c709675ed844
JD-Rosales <rosales.jake1999@gmail.com>
2024-08-22 15:33:18 +0800

    refactor(frontend): remove getScreen and move component bb-87

--- apps/frontend/src/pages/quiz/quiz.tsx
+++ apps/frontend/src/pages/quiz/quiz.tsx
@@ -1,11 +1,33 @@
-import { QuizIntroductionScreen } from "./components/components.js";
+import lifeBalanceWheel from "~/assets/img/life-balance-wheel.svg";
+import { Button } from "~/libs/components/components.js";
+
+import styles from "./styles.module.css";
 
 const Quiz: React.FC = () => {
-	const getScreen = (): React.ReactNode => {
-		return <QuizIntroductionScreen />;
-	};
+	return (
+		<div className={styles["container"]}>
+			<div className={styles["introduction"]}>
+				<h1 className={styles["header"]}>
+					Craft your personal Life Balance Wheel!
+				</h1>
 
-	return <>{getScreen()}</>;
+				<img
+					alt="Life Balance Wheel"
+					className={styles["wheel-img"]}
+					src={lifeBalanceWheel}
+				/>
+
+				<p className={styles["text"]}>
+					Answer a few questions to find out which areas of your life are
+					outstanding and which areas you are missing out on
+				</p>
+
+				<div className={styles["btn"]}>
+					<Button isFluid label="CONTINUE" type="button" />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export { Quiz };
