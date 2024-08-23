41f111b63f973cf7cdff97328e013dd146e24bc4
Andrii Levin <andriilevin@gmail.com>
2024-08-23 15:10:13 +0100

    refactor(frontend): update data source for user in Header component  bb-91

--- apps/frontend/src/libs/components/header/header.tsx
+++ apps/frontend/src/libs/components/header/header.tsx
@@ -1,16 +1,14 @@
-import React from "react";
-
 import { useAppSelector } from "~/libs/hooks/hooks.js";
 
 import { UserHeaderInformation } from "./components/components.js";
 import styles from "./styles.module.css";
 
 const Header: React.FC = () => {
-	const user = useAppSelector(({ users }) => users.user);
+	const user = useAppSelector(({ auth }) => auth.user);
 
 	return (
 		<header className={styles["header"]}>
-			{user && <UserHeaderInformation user={user} />}
+			{user && <UserHeaderInformation user={user.user} />}
 		</header>
 	);
 };
