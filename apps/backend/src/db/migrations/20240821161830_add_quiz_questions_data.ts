0a133483f7da04a48b5794f2623921aed7664c00
luiqor <vohnenna@gmail.com>
2024-08-23 08:30:04 +0300

    fix(backend):  use Maps instead of objects for stroing categoryToQuestions, questionToAnswers bb-59

--- apps/backend/src/db/migrations/20240821161830_add_quiz_questions_data.ts
+++ apps/backend/src/db/migrations/20240821161830_add_quiz_questions_data.ts
@@ -35,48 +35,72 @@ const ColumnName = {
 	UPDATED_AT: "updated_at",
 } as const;
 
-const QUIZ_QUESTIONS = {
-	[Categories.FREE_TIME]: [
-		"How satisfied are you with the amount of free time you have?",
-		"How often do you engage in hobbies or leisure activities?",
-		"How much do you enjoy your free time?",
+const categoryToQuestions = new Map<string, string[]>([
+	[
+		Categories.FREE_TIME,
+		[
+			"How satisfied are you with the amount of free time you have?",
+			"How often do you engage in hobbies or leisure activities?",
+			"How much do you enjoy your free time?",
+		],
 	],
-	[Categories.FRIENDS]: [
-		"How satisfied are you with the quality of your friendships?",
-		"How often do you spend time with your friends?",
-		"How supported do you feel by your friends?",
+	[
+		Categories.FRIENDS,
+		[
+			"How satisfied are you with the quality of your friendships?",
+			"How often do you spend time with your friends?",
+			"How supported do you feel by your friends?",
+		],
 	],
-	[Categories.LOVE]: [
-		"How satisfied are you with your romantic relationship(s)?",
-		"How well do you communicate with your partner?",
-		"How much quality time do you spend with your partner?",
+	[
+		Categories.LOVE,
+		[
+			"How satisfied are you with your romantic relationship(s)?",
+			"How well do you communicate with your partner?",
+			"How much quality time do you spend with your partner?",
+		],
 	],
-	[Categories.MENTAL]: [
-		"How would you rate your overall mental health?",
-		"How well do you manage stress and anxiety?",
-		"How often do you practice mindfulness or other mental health techniques?",
+	[
+		Categories.MENTAL,
+		[
+			"How would you rate your overall mental health?",
+			"How well do you manage stress and anxiety?",
+			"How often do you practice mindfulness or other mental health techniques?",
+		],
 	],
-	[Categories.MONEY]: [
-		"How satisfied are you with your financial situation?",
-		"How well do you manage your personal finances?",
-		"How confident are you in your financial future?",
+	[
+		Categories.MONEY,
+		[
+			"How satisfied are you with your financial situation?",
+			"How well do you manage your personal finances?",
+			"How confident are you in your financial future?",
+		],
 	],
-	[Categories.PHYSICAL]: [
-		"How often do you engage in physical exercise each week?",
-		"How would you rate your overall physical health and fitness?",
-		"How balanced is your diet and nutrition?",
+	[
+		Categories.PHYSICAL,
+		[
+			"How often do you engage in physical exercise each week?",
+			"How would you rate your overall physical health and fitness?",
+			"How balanced is your diet and nutrition?",
+		],
 	],
-	[Categories.SPIRITUAL]: [
-		"How connected do you feel to your spiritual beliefs or practices?",
-		"How often do you engage in spiritual or reflective practices (e.g., meditation, prayer)?",
-		"How much fulfillment do you derive from your spiritual practices?",
+	[
+		Categories.SPIRITUAL,
+		[
+			"How connected do you feel to your spiritual beliefs or practices?",
+			"How often do you engage in spiritual or reflective practices (e.g., meditation, prayer)?",
+			"How much fulfillment do you derive from your spiritual practices?",
+		],
 	],
-	[Categories.WORK]: [
-		"How satisfied are you with your current job or career?",
-		"How well do you manage work-related stress?",
-		"How effectively do you balance your work responsibilities with personal life?",
+	[
+		Categories.WORK,
+		[
+			"How satisfied are you with your current job or career?",
+			"How well do you manage work-related stress?",
+			"How effectively do you balance your work responsibilities with personal life?",
+		],
 	],
-};
+]);
 
 async function up(knex: Knex): Promise<void> {
 	const categories: Category[] = await knex<Category>(TableName.CATEGORIES)
@@ -87,19 +111,20 @@ async function up(knex: Knex): Promise<void> {
 		categories.map((category) => [category.name, category.id]),
 	);
 
-	const quizQuestions: QuizQuestion[] = Object.entries(QUIZ_QUESTIONS).flatMap(
-		([category, questionLabels]) =>
-			questionLabels.map((label) => ({
-				category_id: categoryIdByLabel[category] as number,
-				label,
-			})),
+	const quizQuestions: QuizQuestion[] = [
+		...categoryToQuestions.entries(),
+	].flatMap(([category, questionLabels]) =>
+		questionLabels.map((label) => ({
+			category_id: categoryIdByLabel[category] as number,
+			label,
+		})),
 	);
 
 	await knex<QuizQuestion>(TableName.QUIZ_QUESTIONS).insert(quizQuestions);
 }
 
 function down(knex: Knex): Promise<void> {
-	const questionLabels = Object.values(QUIZ_QUESTIONS).flat();
+	const questionLabels = [...categoryToQuestions.values()].flat();
 
 	return knex<QuizQuestion>(TableName.QUIZ_QUESTIONS)
 		.whereIn(ColumnName.LABEL, questionLabels)
--- apps/backend/src/db/migrations/20240822055434_add_quiz_answers_data.ts
+++ apps/backend/src/db/migrations/20240822055434_add_quiz_answers_data.ts
@@ -26,29 +26,33 @@ const ColumnName = {
 const VALUE_START_INDEX_OFFSET = 1;
 const VALUE_MULTIPLIER = 2;
 
-const QUIZ_ANSWERS = {
-	["How balanced is your diet and nutrition?"]: [
-		"Very unbalanced",
-		"Unbalanced",
-		"Neutral",
-		"Balanced",
-		"Very balanced",
+const questionToAnswers = new Map<string, string[]>([
+	[
+		"How balanced is your diet and nutrition?",
+		["Very unbalanced", "Unbalanced", "Neutral", "Balanced", "Very balanced"],
 	],
-	["How confident are you in your financial future?"]: [
-		"Very unconfident",
-		"Unconfident",
-		"Neutral",
-		"Confident",
-		"Very confident",
+	[
+		"How confident are you in your financial future?",
+		[
+			"Very unconfident",
+			"Unconfident",
+			"Neutral",
+			"Confident",
+			"Very confident",
+		],
 	],
-	["How connected do you feel to your spiritual beliefs or practices?"]: [
-		"Not connected at all",
-		"Slightly connected",
-		"Neutral",
-		"Connected",
-		"Very connected",
+	[
+		"How connected do you feel to your spiritual beliefs or practices?",
+		[
+			"Not connected at all",
+			"Slightly connected",
+			"Neutral",
+			"Connected",
+			"Very connected",
+		],
 	],
-	["How effectively do you balance your work responsibilities with personal life?"]:
+	[
+		"How effectively do you balance your work responsibilities with personal life?",
 		[
 			"Very ineffectively",
 			"Ineffectively",
@@ -56,42 +60,21 @@ const QUIZ_ANSWERS = {
 			"Effectively",
 			"Very effectively",
 		],
-	["How much do you enjoy your free time?"]: [
-		"Not at all",
-		"Slightly",
-		"Moderately",
-		"Much",
-		"Very much",
 	],
-	["How much fulfillment do you derive from your spiritual practices?"]: [
-		"None",
-		"Little",
-		"Moderate",
-		"Much",
-		"Very much",
+	[
+		"How much do you enjoy your free time?",
+		["Not at all", "Slightly", "Moderately", "Much", "Very much"],
 	],
-	["How much quality time do you spend with your partner?"]: [
-		"Very little",
-		"Little",
-		"Moderate",
-		"Much",
-		"Very much",
+	[
+		"How much fulfillment do you derive from your spiritual practices?",
+		["None", "Little", "Moderate", "Much", "Very much"],
 	],
-	["How often do you engage in hobbies or leisure activities?"]: [
-		"Rarely or never",
-		"Once a month",
-		"Once a week",
-		"Several times a week",
-		"Daily",
+	[
+		"How much quality time do you spend with your partner?",
+		["Very little", "Little", "Moderate", "Much", "Very much"],
 	],
-	["How often do you engage in physical exercise each week?"]: [
-		"Rarely or never",
-		"Once a week",
-		"Two to three times a week",
-		"Four to five times a week",
-		"Daily",
-	],
-	["How often do you engage in spiritual or reflective practices (e.g., meditation, prayer)?"]:
+	[
+		"How often do you engage in hobbies or leisure activities?",
 		[
 			"Rarely or never",
 			"Once a month",
@@ -99,7 +82,19 @@ const QUIZ_ANSWERS = {
 			"Several times a week",
 			"Daily",
 		],
-	["How often do you practice mindfulness or other mental health techniques?"]:
+	],
+	[
+		"How often do you engage in physical exercise each week?",
+		[
+			"Rarely or never",
+			"Once a week",
+			"Two to three times a week",
+			"Four to five times a week",
+			"Daily",
+		],
+	],
+	[
+		"How often do you engage in spiritual or reflective practices (e.g., meditation, prayer)?",
 		[
 			"Rarely or never",
 			"Once a month",
@@ -107,111 +102,125 @@ const QUIZ_ANSWERS = {
 			"Several times a week",
 			"Daily",
 		],
-	["How often do you spend time with your friends?"]: [
-		"Rarely or never",
-		"Once a month",
-		"Once a week",
-		"Several times a week",
-		"Daily",
 	],
-	["How satisfied are you with the amount of free time you have?"]: [
-		"Very dissatisfied",
-		"Dissatisfied",
-		"Neutral",
-		"Satisfied",
-		"Very satisfied",
+	[
+		"How often do you practice mindfulness or other mental health techniques?",
+		[
+			"Rarely or never",
+			"Once a month",
+			"Once a week",
+			"Several times a week",
+			"Daily",
+		],
 	],
-	["How satisfied are you with the quality of your friendships?"]: [
-		"Very dissatisfied",
-		"Dissatisfied",
-		"Neutral",
-		"Satisfied",
-		"Very satisfied",
+	[
+		"How often do you spend time with your friends?",
+		[
+			"Rarely or never",
+			"Once a month",
+			"Once a week",
+			"Several times a week",
+			"Daily",
+		],
 	],
-	["How satisfied are you with your current job or career?"]: [
-		"Very dissatisfied",
-		"Dissatisfied",
-		"Neutral",
-		"Satisfied",
-		"Very satisfied",
+	[
+		"How satisfied are you with the amount of free time you have?",
+		[
+			"Very dissatisfied",
+			"Dissatisfied",
+			"Neutral",
+			"Satisfied",
+			"Very satisfied",
+		],
 	],
-	["How satisfied are you with your financial situation?"]: [
-		"Very dissatisfied",
-		"Dissatisfied",
-		"Neutral",
-		"Satisfied",
-		"Very satisfied",
+	[
+		"How satisfied are you with the quality of your friendships?",
+		[
+			"Very dissatisfied",
+			"Dissatisfied",
+			"Neutral",
+			"Satisfied",
+			"Very satisfied",
+		],
 	],
-	["How satisfied are you with your romantic relationship(s)?"]: [
-		"Very dissatisfied",
-		"Dissatisfied",
-		"Neutral",
-		"Satisfied",
-		"Very satisfied",
+	[
+		"How satisfied are you with your current job or career?",
+		[
+			"Very dissatisfied",
+			"Dissatisfied",
+			"Neutral",
+			"Satisfied",
+			"Very satisfied",
+		],
 	],
-	["How supported do you feel by your friends?"]: [
-		"Not supported at all",
-		"Slightly supported",
-		"Neutral",
-		"Supported",
-		"Very supported",
+	[
+		"How satisfied are you with your financial situation?",
+		[
+			"Very dissatisfied",
+			"Dissatisfied",
+			"Neutral",
+			"Satisfied",
+			"Very satisfied",
+		],
 	],
-	["How well do you communicate with your partner?"]: [
-		"Very poorly",
-		"Poorly",
-		"Neutral",
-		"Well",
-		"Very well",
+	[
+		"How satisfied are you with your romantic relationship(s)?",
+		[
+			"Very dissatisfied",
+			"Dissatisfied",
+			"Neutral",
+			"Satisfied",
+			"Very satisfied",
+		],
 	],
-	["How well do you manage stress and anxiety?"]: [
-		"Very poorly",
-		"Poorly",
-		"Neutral",
-		"Well",
-		"Very well",
+	[
+		"How supported do you feel by your friends?",
+		[
+			"Not supported at all",
+			"Slightly supported",
+			"Neutral",
+			"Supported",
+			"Very supported",
+		],
 	],
-	["How well do you manage work-related stress?"]: [
-		"Very poorly",
-		"Poorly",
-		"Fair",
-		"Well",
-		"Very well",
+	[
+		"How well do you communicate with your partner?",
+		["Very poorly", "Poorly", "Neutral", "Well", "Very well"],
 	],
-	["How well do you manage your personal finances?"]: [
-		"Very poorly",
-		"Poorly",
-		"Neutral",
-		"Well",
-		"Very well",
+	[
+		"How well do you manage stress and anxiety?",
+		["Very poorly", "Poorly", "Neutral", "Well", "Very well"],
 	],
-	["How would you rate your overall mental health?"]: [
-		"Very poor",
-		"Poor",
-		"Neutral",
-		"Good",
-		"Excellent",
+	[
+		"How well do you manage work-related stress?",
+		["Very poorly", "Poorly", "Fair", "Well", "Very well"],
 	],
-	["How would you rate your overall physical health and fitness?"]: [
-		"Poor",
-		"Fair",
-		"Good",
-		"Very good",
-		"Excellent",
+	[
+		"How well do you manage your personal finances?",
+		["Very poorly", "Poorly", "Neutral", "Well", "Very well"],
 	],
-};
+	[
+		"How would you rate your overall mental health?",
+		["Very poor", "Poor", "Neutral", "Good", "Excellent"],
+	],
+	[
+		"How would you rate your overall physical health and fitness?",
+		["Poor", "Fair", "Good", "Very good", "Excellent"],
+	],
+]);
 
 async function up(knex: Knex): Promise<void> {
 	const questions: QuizQuestion[] = await knex<QuizQuestion>(
 		TableName.QUIZ_QUESTIONS,
 	)
 		.select(ColumnName.ID, ColumnName.LABEL)
-		.whereIn(ColumnName.LABEL, Object.keys(QUIZ_ANSWERS));
+		.whereIn(ColumnName.LABEL, [...questionToAnswers.keys()]);
 
 	const questionIdByLabel: Record<string, number> = Object.fromEntries(
 		questions.map((question) => [question.label, question.id]),
 	);
 
-	const quizAnswers: QuizAnswer[] = Object.entries(QUIZ_ANSWERS).flatMap(
+	const quizAnswers: QuizAnswer[] = [...questionToAnswers.entries()].flatMap(
 		([questionLabel, answers]) =>
 			answers.map((answerLabel, index) => ({
 				[ColumnName.LABEL]: answerLabel,
@@ -225,7 +234,7 @@ async function up(knex: Knex): Promise<void> {
 }
 
 function down(knex: Knex): Promise<void> {
-	const questionLabels = Object.keys(QUIZ_ANSWERS);
+	const questionLabels = [...questionToAnswers.keys()];
 
 	return knex<QuizAnswer>(TableName.QUIZ_ANSWERS)
 		.whereIn(
