d71b59151f7620c2dcfa638484bc3eeec3123619
ihorLysak <lysak.ihor4221@gmail.com>
2024-08-24 14:00:53 +0300

    refactor: rewritten icon name to svg map bb-90

--- apps/frontend/src/libs/maps/icon-name-to-svg.map.ts
+++ apps/frontend/src/libs/maps/icon-name-to-svg.map.ts
@@ -2,9 +2,9 @@ import wheelOfBalanceActive from "~/assets/img/wheel-of-balance-active.svg";
 import wheelOfBalanceIncative from "~/assets/img/wheel-of-balance-inactive.svg";
 import { IconName } from "~/libs/types/types.js";
 
-const iconNameToSvg = new Map<IconName, string>();
-
-iconNameToSvg.set("my-wheel-active", wheelOfBalanceActive);
-iconNameToSvg.set("my-wheel-inactive", wheelOfBalanceIncative);
+const iconNameToSvg = new Map<IconName, string>([
+	["my-wheel-active", wheelOfBalanceActive],
+	["my-wheel-inactive", wheelOfBalanceIncative],
+]);
 
 export { iconNameToSvg };
