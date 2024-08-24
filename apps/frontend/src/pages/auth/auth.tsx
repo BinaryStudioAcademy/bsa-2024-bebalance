e95c7255c530717a9317c4a18975d090691e8298
iqbalalayubbi <iqbal.is415@gmail.com>
2024-08-24 17:20:57 +0700

    fix: make same style in signup with signin bb-60

--- apps/frontend/src/pages/auth/auth.tsx
+++ apps/frontend/src/pages/auth/auth.tsx
@@ -1,5 +1,3 @@
-import authIllustrationLeft from "~/assets/img/auth-illustration-left.svg";
-import authIllustrationRight from "~/assets/img/auth-illustration-right.svg";
 import { AppRoute } from "~/libs/enums/enums.js";
 import {
 	useAppDispatch,
@@ -13,7 +11,6 @@ import {
 } from "~/modules/users/users.js";
 
 import { SignInForm, SignUpForm } from "./components/components.js";
-import styles from "./style.module.css";
 
 const Auth: React.FC = () => {
 	const dispatch = useAppDispatch();
@@ -48,23 +45,8 @@ const Auth: React.FC = () => {
 
 	return (
 		<>
-			<main className={styles["auth-container"]}>
-				<section className={styles["form-container"]}>
-					{getScreen(pathname)}
-				</section>
-				<section className={styles["illustration-container"]}>
-					<img
-						alt="background"
-						className={styles["img-left"]}
-						src={authIllustrationLeft}
-					/>
-					<h1 className={styles["title"]}>Logo</h1>
-					<img
-						alt="background"
-						className={styles["img-right"]}
-						src={authIllustrationRight}
-					/>
-				</section>
+			<main>
+				<section>{getScreen(pathname)}</section>
 			</main>
 		</>
 	);
--- apps/frontend/src/pages/auth/components/sign-in-form/sign-in-form.tsx
+++ apps/frontend/src/pages/auth/components/sign-in-form/sign-in-form.tsx
@@ -1,6 +1,7 @@
+import rippleEffectBg from "~/assets/img/ripple-effect-bg.svg";
+import rippleEffectBg2 from "~/assets/img/ripple-effect-bg2.svg";
 import { Button, Input, Link } from "~/libs/components/components.js";
 import { AppRoute } from "~/libs/enums/enums.js";
-import { getValidClassNames } from "~/libs/helpers/helpers.js";
 import { useAppForm, useCallback } from "~/libs/hooks/hooks.js";
 import {
 	type UserSignInRequestDto,
@@ -28,46 +29,64 @@ const SignInForm: React.FC<Properties> = ({ onSubmit }: Properties) => {
 	);
 
 	return (
-		<section className={styles["signup-container"]}>
-			<div className={styles["logo-container"]}>
-				<div
-					className={getValidClassNames(
-						styles["circle"],
-						styles["circle-pink"],
-					)}
-				/>
-				<h1>LOGO</h1>
-			</div>
-			<h3 className={styles["title-form"]}>Sign In</h3>
-			<span className={styles["auth-info"]}>
-				No account? Go to
-				<Link to={AppRoute.SIGN_UP}> Create an account</Link>
-			</span>
-			<form onSubmit={handleFormSubmit}>
-				<div className={styles["input-groups"]}>
+		<div className={styles["container"]}>
+			<div className={styles["form-container"]}>
+				<form className={styles["form"]} onSubmit={handleFormSubmit}>
+					<div className={styles["form-header"]}>
+						<div className={styles["form-header__logo-container"]}>
+							<div className={styles["form-header__logo"]} />
+							<span className={styles["form-header__logo-text"]}>LOGO</span>
+						</div>
+
+						<h1 className={styles["form-header__text"]}>SIGN IN</h1>
+						<span className={styles["form-header__sub-text"]}>
+							No account? Go to{" "}
+							<Link to={AppRoute.SIGN_UP}>Create an account</Link>
+						</span>
+					</div>
+
 					<Input
 						control={control}
 						errors={errors}
 						label="Email"
 						name="email"
-						placeholder="email"
+						placeholder="name@gmail.com"
 						type="email"
 					/>
+
 					<Input
 						control={control}
 						errors={errors}
 						label="Password"
 						name="password"
-						placeholder="password"
-						type="password"
+						placeholder="*******"
+						type="text"
 					/>
-				</div>
-				<div className={styles["footer-groups"]}>
+
 					<Button label="SIGN IN" type="submit" variant="dark" />
-					<Link to="/">Forgot password?</Link>
-				</div>
-			</form>
-		</section>
+				</form>
+			</div>
+
+			<div className={styles["logo-container"]}>
+				<span className={styles["logo"]}>LOGO</span>
+			</div>
+
+			<img
+				alt="ripple-effect-bg"
+				className={styles["ripple-effect__background1"]}
+				src={rippleEffectBg}
+			/>
+			<img
+				alt="ripple-effect-bg"
+				className={styles["ripple-effect__background2"]}
+				src={rippleEffectBg2}
+			/>
+
+			<div className={styles["circle-gradient1"]} />
+			<div className={styles["circle-gradient2"]} />
+			<div className={styles["circle-gradient3"]} />
+			<div className={styles["circle-gradient4"]} />
+		</div>
 	);
 };
 
