c57e74282c48ccbe25be58b807437a9c5d2d84e3
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-08-25 14:24:32 +0300

    feat(frontend): add analizing component bb-135

---
+++ apps/frontend/src/pages/quiz/libs/components/analizing/analizing.tsx
@@ -0,0 +1,42 @@
+import { Button } from "~/libs/components/components.js";
+
+import styles from "./styles.module.css";
+
+type Properties = {
+	onNext: () => void;
+};
+
+const Analizing: React.FC<Properties> = ({ onNext }: Properties) => {
+	return (
+		<div className={styles["page-container"]}>
+			<div className={styles["border-container"]}>
+				<div className={styles["content-container"]}>
+					<h1 className={styles["title"]}>We’re Analyzing Your Journey!</h1>
+					<div className={styles["text"]}>
+						<p>
+							Thank you for sharing your insights! We’re currently processing
+							your responses to create a personalized path just for you. This is
+							where the magic begins—we’re using your input to tailor the
+							experience, offering you the guidance and motivation you need to
+							achieve a balanced, fulfilling life.
+						</p>
+						<p>
+							Hang tight while we set things up! In just a moment, you’ll dive
+							into the areas that matter most to you, and together, we’ll start
+							making progress toward your goals. Your journey to a better life
+							starts now!
+						</p>
+					</div>
+					<Button
+						label="Let’s continue"
+						onClick={onNext}
+						type="button"
+						variant="dark"
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export { Analizing };
