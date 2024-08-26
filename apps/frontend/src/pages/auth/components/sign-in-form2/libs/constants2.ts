a0798a1b2572fa2d7ddd848d206eaa18285aeaf1
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-08-26 12:33:28 +0300

    refactor(frontend): remove unnecessary bb-145

--- apps/frontend/src/pages/auth/components/sign-in-form2/libs/constants2.ts
+++
@@ -1,8 +0,0 @@
-import { type UserSignInRequestDto } from "~/modules/users/users.js";
-
-const DEFAULT_SIGN_IN_PAYLOAD: UserSignInRequestDto = {
-	email: "",
-	password: "",
-};
-
-export { DEFAULT_SIGN_IN_PAYLOAD };
--- apps/frontend/src/pages/not-found/not-found.tsx
+++ apps/frontend/src/pages/not-found/not-found.tsx
@@ -1,36 +1,18 @@
-import { useCallback } from "react";
-import { type UserSignInRequestDto } from "shared";
-
 import { Link } from "~/libs/components/components.js";
 import { AppRoute } from "~/libs/enums/enums.js";
-import { useAppDispatch } from "~/libs/hooks/hooks.js";
-import { actions as authActions } from "~/modules/auth/auth.js";
 
-import { SignInForm } from "../auth/components/sign-in-form2/sign-in-form2.js";
 import styles from "./styles.module.css";
 
-const NotFound: React.FC = () => {
-	const dispatch = useAppDispatch();
-
-	const handleSignInSubmit = useCallback(
-		(payload: UserSignInRequestDto): void => {
-			void dispatch(authActions.signIn(payload));
-		},
-		[dispatch],
-	);
-
-	return (
-		<div className={styles["pageContainer"]}>
-			<h1 className={styles["title"]}>404 - Page Not Found</h1>
-			<p className={styles["text"]}>
-				The page you are looking for does not exist.
-			</p>
-			<SignInForm onSubmit={handleSignInSubmit} />
-			<Link to={AppRoute.ROOT} type="button">
-				Go to Homepage
-			</Link>
-		</div>
-	);
-};
+const NotFound: React.FC = () => (
+	<div className={styles["pageContainer"]}>
+		<h1 className={styles["title"]}>404 - Page Not Found</h1>
+		<p className={styles["text"]}>
+			The page you are looking for does not exist.
+		</p>
+		<Link to={AppRoute.ROOT} type="button">
+			Go to Homepage
+		</Link>
+	</div>
+);
 
 export { NotFound };
