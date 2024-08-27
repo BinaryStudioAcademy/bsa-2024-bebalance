ff341130fbbc32e257931dba77dd58ce196dfb17
Andrii Levin <andriilevin@gmail.com>
2024-08-27 07:48:36 +0100

    feat(frontend/backend): added ProgressBar steps helper bb-134

--- apps/frontend/src/libs/helpers/helpers.ts
+++ apps/frontend/src/libs/helpers/helpers.ts
@@ -1,2 +1,6 @@
 export { getValidClassNames } from "./get-valid-class-names/get-valid-class-names.js";
+export {
+	advanceStep,
+	createSteps,
+} from "./progress-bar-steps/progress-bar-steps.js";
 export { configureString } from "shared";
