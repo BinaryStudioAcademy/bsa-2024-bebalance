21df47db70d6cb97f80c8ddfb64826aea0468e97
Andrii Levin <andriilevin@gmail.com>
2024-08-27 07:23:43 +0100

    feat(frontend/backend): added OnboardingAnswerModel and OnboardingAnswerEntity models   bb-134

---
+++ apps/backend/src/modules/surveys/onboarding/onboarding-answer.entuty.ts
@@ -0,0 +1,99 @@
+import { type Entity } from "~/libs/types/types.js";
+
+class OnboardingAnswerEntity implements Entity {
+	private createdAt: string;
+
+	private id: null | number;
+
+	private label: string;
+
+	private questionId: number;
+
+	private updatedAt: string;
+
+	private constructor({
+		createdAt,
+		id,
+		label,
+		questionId,
+		updatedAt,
+	}: {
+		createdAt: string;
+		id: null | number;
+		label: string;
+		questionId: number;
+		updatedAt: string;
+	}) {
+		this.label = label;
+		this.createdAt = createdAt;
+		this.id = id;
+		this.questionId = questionId;
+		this.updatedAt = updatedAt;
+	}
+
+	public static initialize({
+		createdAt,
+		id,
+		label,
+		questionId,
+		updatedAt,
+	}: {
+		createdAt: string;
+		id: number;
+		label: string;
+		questionId: number;
+		updatedAt: string;
+	}): OnboardingAnswerEntity {
+		return new OnboardingAnswerEntity({
+			createdAt,
+			id,
+			label,
+			questionId,
+			updatedAt,
+		});
+	}
+
+	public static initializeNew({
+		label,
+		questionId,
+	}: {
+		label: string;
+		questionId: number;
+	}): OnboardingAnswerEntity {
+		return new OnboardingAnswerEntity({
+			createdAt: "",
+			id: null,
+			label,
+			questionId,
+			updatedAt: "",
+		});
+	}
+
+	public toNewObject(): {
+		label: string;
+		questionId: number;
+	} {
+		return {
+			label: this.label,
+			questionId: this.questionId,
+		};
+	}
+
+	public toObject(): {
+		createdAt: string;
+		id: number;
+		label: string;
+		questionId: number;
+		updatedAt: string;
+	} {
+		return {
+			createdAt: this.createdAt,
+			id: this.id as number,
+			label: this.label,
+			questionId: this.questionId,
+			updatedAt: this.updatedAt,
+		};
+	}
+}
+
+export { OnboardingAnswerEntity };
