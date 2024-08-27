539a9774785245bb7d9fadf59ccae78e0dc45b5b
luiqor <vohnenna@gmail.com>
2024-08-27 17:37:26 +0300

    fix(backend/shared): update error messages bb-166

--- packages/shared/src/modules/quiz/libs/enums/quiz-validation-message.enum.ts
+++ packages/shared/src/modules/quiz/libs/enums/quiz-validation-message.enum.ts
@@ -1,11 +1,10 @@
 import { QuizValidationRule } from "./quiz-validation-rule.enum.js";
 
 const QuizValidationMessage = {
-	ID_ARRAY_REQUIRED: "Array of id's is required",
 	NUMBER_ID_REQUIRED: "Required answer IDs must be numbers.",
-	QUESTIONS_LENGTH: `Password must be at most ${String(
+	QUESTIONS_LENGTH: `Answers for all ${String(
 		QuizValidationRule.QUESTIONS_LENGTH,
-	)} characters long`,
+	)} questions are required`,
 	UNIQUE_ANSWERS: "Answers must be unique",
 } as const;
 
--- packages/shared/src/modules/quiz/libs/validation-schemas/quiz-user-answers.validation-schema.ts
+++ packages/shared/src/modules/quiz/libs/validation-schemas/quiz-user-answers.validation-schema.ts
@@ -3,17 +3,12 @@ import { z } from "zod";
 import { QuizValidationMessage, QuizValidationRule } from "../enums/enums.js";
 
 type QuizUserAnswersValidationSchema = {
-	answerIds: z.ZodEffects<
-		z.ZodArray<z.ZodNumber, "atleastone">,
-		number[],
-		number[]
-	>;
+	answerIds: z.ZodEffects<z.ZodArray<z.ZodNumber>, number[], number[]>;
 };
 
 const quizUserAnswers = z.object<QuizUserAnswersValidationSchema>({
 	answerIds: z
 		.array(z.number({ message: QuizValidationMessage.NUMBER_ID_REQUIRED }))
-		.nonempty({ message: QuizValidationMessage.ID_ARRAY_REQUIRED })
 		.length(QuizValidationRule.QUESTIONS_LENGTH, {
 			message: QuizValidationMessage.QUESTIONS_LENGTH,
 		})
