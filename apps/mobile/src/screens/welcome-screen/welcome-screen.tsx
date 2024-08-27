2764039097481716288147e88c900b7e8fe6e7c6
Olga <okatranis@gmail.com>
2024-08-27 11:02:14 +0300

    feat: placeholder route to bottom-tabs-screen, bb-128

--- apps/mobile/src/screens/welcome-screen/welcome-screen.tsx
+++ apps/mobile/src/screens/welcome-screen/welcome-screen.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 
-import { Text, View } from "~/libs/components/components";
+import { Link, Text, View } from "~/libs/components/components";
+import { RootScreenName } from "~/libs/enums/navigation/root-screen-name.enum";
 
 const WelcomeScreen: React.FC = () => {
 	return (
 		<View>
 			<Text>Welcome to the App!</Text>
+			<Link
+				label="To bottom tabs nav"
+				to={`/${RootScreenName.BOTTOM_TABS_NAVIGATOR}`}
+			/>
 		</View>
 	);
 };
