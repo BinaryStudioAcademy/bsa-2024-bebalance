ebbfd7b9fadf485662205659806fde1e821d78a2
Andrii Levin <andriilevin@gmail.com>
2024-08-27 07:23:08 +0100

    feat(frontend/backend): added OnboardingAnswerModel and OnboardingAnswerEntity models   bb-134

--- apps/backend/src/libs/modules/database/libs/enums/database-table-name.enum.ts
+++ apps/backend/src/libs/modules/database/libs/enums/database-table-name.enum.ts
@@ -1,6 +1,8 @@
 const DatabaseTableName = {
 	CATEGORIES: "categories",
 	MIGRATIONS: "migrations",
+	ONBOARDING_ANSWERS: "onboarding_answers",
+	ONBOARDING_QUESTIONS: "onboarding_questions",
 	USER_DETAILS: "user_details",
 	USERS: "users",
 } as const;
---
+++ apps/backend/src/modules/surveys/onboarding/onboarding-answer.model.ts
@@ -0,0 +1,33 @@
+import { Model, RelationMappings } from "objection";
+
+import {
+	AbstractModel,
+	DatabaseTableName,
+} from "~/libs/modules/database/database.js";
+
+import { OnboardingQuestionModel } from "./onboarding-question.model.js";
+
+class OnboardingAnswerModel extends AbstractModel {
+	public label!: string;
+
+	public questionId!: number;
+
+	static get relationMappings(): RelationMappings {
+		return {
+			question: {
+				join: {
+					from: `${DatabaseTableName.ONBOARDING_ANSWERS}.question_id`,
+					to: `${DatabaseTableName.ONBOARDING_QUESTIONS}.id`,
+				},
+				modelClass: OnboardingQuestionModel,
+				relation: Model.BelongsToOneRelation,
+			},
+		};
+	}
+
+	public static override get tableName(): string {
+		return DatabaseTableName.ONBOARDING_ANSWERS;
+	}
+}
+
+export { OnboardingAnswerModel };
