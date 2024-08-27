6748597e43114a4a31c22ac0d1c9541ee89bd522
Andrii Levin <andriilevin@gmail.com>
2024-08-27 07:24:55 +0100

    feat(frontend/backend): added OnboardingQuestionEntity and OnboardingQuestionModel models   bb-134

---
+++ apps/backend/src/modules/surveys/onboarding/onboarding-question.entity.ts
@@ -0,0 +1,100 @@
+import { type Entity } from "~/libs/types/types.js";
+
+import { type SurveyAnswerDto } from "../libs/types/types.js";
+import { OnboardingAnswerEntity } from "./onboarding-answer.entuty.js";
+
+class OnboardingQuestionEntity implements Entity {
+	private answers: OnboardingAnswerEntity[];
+
+	private createdAt: string;
+
+	private id: null | number;
+
+	private label: string;
+
+	private updatedAt: string;
+
+	private constructor({
+		answers,
+		createdAt,
+		id,
+		label,
+		updatedAt,
+	}: {
+		answers: OnboardingAnswerEntity[];
+		createdAt: string;
+		id: null | number;
+		label: string;
+		updatedAt: string;
+	}) {
+		this.answers = answers;
+		this.createdAt = createdAt;
+		this.id = id;
+		this.label = label;
+		this.updatedAt = updatedAt;
+	}
+
+	public static initialize({
+		answers,
+		createdAt,
+		id,
+		label,
+		updatedAt,
+	}: {
+		answers: OnboardingAnswerEntity[];
+		createdAt: string;
+		id: number;
+		label: string;
+		updatedAt: string;
+	}): OnboardingQuestionEntity {
+		return new OnboardingQuestionEntity({
+			answers,
+			createdAt,
+			id,
+			label,
+			updatedAt,
+		});
+	}
+
+	public static initializeNew({
+		label,
+	}: {
+		label: string;
+	}): OnboardingQuestionEntity {
+		return new OnboardingQuestionEntity({
+			answers: [],
+			createdAt: "",
+			id: null,
+			label,
+			updatedAt: "",
+		});
+	}
+
+	public toNewObject(): {
+		label: string;
+	} {
+		return {
+			label: this.label,
+		};
+	}
+
+	public toObject(): {
+		answers: SurveyAnswerDto[];
+		createdAt: string;
+		id: number;
+		label: string;
+		updatedAt: string;
+	} {
+		return {
+			answers: this.answers.map((answer) => {
+				return answer.toObject();
+			}),
+			createdAt: this.createdAt,
+			id: this.id as number,
+			label: this.label,
+			updatedAt: this.updatedAt,
+		};
+	}
+}
+
+export { OnboardingQuestionEntity };
---
+++ apps/backend/src/modules/surveys/onboarding/onboarding-question.model.ts
@@ -0,0 +1,33 @@
+import { Model, RelationMappings } from "objection";
+
+import {
+	AbstractModel,
+	DatabaseTableName,
+} from "~/libs/modules/database/database.js";
+
+import { OnboardingAnswerModel } from "./onboarding-answer.model.js";
+
+class OnboardingQuestionModel extends AbstractModel {
+	public answers!: OnboardingAnswerModel[];
+
+	public label!: string;
+
+	static get relationMappings(): RelationMappings {
+		return {
+			answers: {
+				join: {
+					from: `${DatabaseTableName.ONBOARDING_QUESTIONS}.id`,
+					to: `${DatabaseTableName.ONBOARDING_ANSWERS}.questionId`,
+				},
+				modelClass: OnboardingAnswerModel,
+				relation: Model.HasManyRelation,
+			},
+		};
+	}
+
+	public static override get tableName(): string {
+		return DatabaseTableName.ONBOARDING_QUESTIONS;
+	}
+}
+
+export { OnboardingQuestionModel };
