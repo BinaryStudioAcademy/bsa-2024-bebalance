1ec0297308d7a330d19d804ead21a7d73dae3f7c
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-08-28 14:45:48 +0300

    refactor(backend): createUserAnswers repository bb-167

--- apps/backend/src/modules/onboarding/onboarding.controller.ts
+++ apps/backend/src/modules/onboarding/onboarding.controller.ts
@@ -9,7 +9,6 @@ import { type Logger } from "~/libs/modules/logger/logger.js";
 
 import { OnboardingApiPath } from "./libs/enums/enums.js";
 import { type UserDto } from "./libs/types/types.js";
-// import { onboardingAnswersValidationSchema } from "../libs/validation-schemas/validation-schemas.js";
 import { type OnboardingService } from "./onboarding.service.js";
 
 class OnboardingController extends BaseController {
@@ -28,11 +27,8 @@ class OnboardingController extends BaseController {
 						user: UserDto;
 					}>,
 				),
-			method: "PUT",
+			method: "POST",
 			path: OnboardingApiPath.ANSWER,
-			// validation: {
-			// 	body: onboardingAnswersValidationSchema,
-			// },
 		});
 	}
 
