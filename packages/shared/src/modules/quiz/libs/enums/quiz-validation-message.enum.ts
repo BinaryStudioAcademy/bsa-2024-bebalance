39c5f7c91183b1ed821abd6710e574c1a32863de
luiqor <vohnenna@gmail.com>
2024-08-28 15:14:29 +0300

    refactor(shared): add dots to quiz validation messages bb-166

--- packages/shared/src/modules/quiz/libs/enums/quiz-validation-message.enum.ts
+++ packages/shared/src/modules/quiz/libs/enums/quiz-validation-message.enum.ts
@@ -4,8 +4,8 @@ const QuizValidationMessage = {
 	NUMBER_ID_REQUIRED: "Required answer IDs must be numbers.",
 	QUESTIONS_LENGTH: `Answers for all ${String(
 		QuizValidationRule.QUESTIONS_LENGTH,
-	)} questions are required`,
-	UNIQUE_ANSWERS: "Answers must be unique",
+	)} questions are required.`,
+	UNIQUE_ANSWERS: "Answers must be unique.",
 } as const;
 
 export { QuizValidationMessage };
