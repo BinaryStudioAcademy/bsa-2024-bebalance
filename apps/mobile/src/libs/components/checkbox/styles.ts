f16dabaf4f7c4b34e2e2d437668214ecc714af4e
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-08-28 17:02:08 +0300

    fix(mobile): fix paddingLeft in Checkbox component bb-174

--- apps/mobile/src/libs/components/checkbox/styles.ts
+++ apps/mobile/src/libs/components/checkbox/styles.ts
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
 		borderRadius: 32,
 		borderWidth: 1,
 		elevation: 5,
-		paddingLeft: 71,
+		paddingLeft: 70,
 		shadowColor: BaseColor.CHECKBOX_BLUE,
 	},
 });
