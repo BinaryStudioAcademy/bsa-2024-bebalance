4efd1f3311a97275989333a7b12875fc2f7386a9
luiqor <vohnenna@gmail.com>
2024-08-31 11:42:00 +0300

    refactor(shared): update QuizValidationMessage enum for better error messages bb-166

--- packages/shared/src/modules/quiz/libs/enums/quiz-validation-message.enum.ts
+++ packages/shared/src/modules/quiz/libs/enums/quiz-validation-message.enum.ts
@@ -1,5 +1,5 @@
 const QuizValidationMessage = {
-	NUMBER_ID_REQUIRED: "Required answer IDs must be numbers.",
+	NUMBER_ID_REQUIRED: "Please enter only numeric answer values.",
 	UNIQUE_ANSWERS: "Answers must be unique.",
 } as const;
 
