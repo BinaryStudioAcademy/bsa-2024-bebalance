e73354d2b9c9b4f38f6a7ee26c2a0ac5404a058f
Andrii Levin <andriilevin@gmail.com>
2024-08-31 23:33:05 +0100

    feat(frontend): add FormFieldProperties bb-134

---
+++ apps/frontend/src/libs/types/form-field-properties.type.ts
@@ -0,0 +1,18 @@
+import {
+	type Control,
+	type FieldPath,
+	type FieldValues,
+} from "react-hook-form";
+
+type FormFieldProperties<T extends FieldValues> = {
+	control: Control<T, null>;
+	name: FieldPath<T>;
+	options?: { label: string; value: string }[];
+};
+
+export { type FormFieldProperties };
+export {
+	type FieldErrors,
+	type FieldPath,
+	type FieldValues,
+} from "react-hook-form";
--- apps/frontend/src/libs/types/types.ts
+++ apps/frontend/src/libs/types/types.ts
@@ -1,12 +1,13 @@
 export { type AsyncThunkConfig } from "./async-thunk-config.type.js";
-export { type IconName } from "./icon-name.type.js";
-export { type SidebarItem } from "./sidebar-item.type.js";
 export {
-	type Control,
 	type FieldErrors,
 	type FieldPath,
 	type FieldValues,
-} from "react-hook-form";
+	type FormFieldProperties,
+} from "./form-field-properties.type.js";
+export { type IconName } from "./icon-name.type.js";
+export { type SidebarItem } from "./sidebar-item.type.js";
+export { type Control } from "react-hook-form";
 export {
 	type OnboardingQuestionResponseDto,
 	type ServerErrorDetail,
