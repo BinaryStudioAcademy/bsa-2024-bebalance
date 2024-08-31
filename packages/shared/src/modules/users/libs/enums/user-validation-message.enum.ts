94b1759d339bf50afa4e3eaf4b776cca565a4563
Olga <okatranis@gmail.com>
2024-08-31 13:22:18 +0300

    feat: update user-validation-message bb-210

--- packages/shared/src/modules/users/libs/enums/user-validation-message.enum.ts
+++ packages/shared/src/modules/users/libs/enums/user-validation-message.enum.ts
@@ -1,10 +1,26 @@
 import { UserValidationRule } from "./user-validation-rule.enum.js";
 
 const UserValidationMessage = {
+	EMAIL_DOMAIN_PART_MAX_LENGTH: `The domain part of the email address must be at most ${String(
+		UserValidationRule.EMAIL_DOMAIN_PART_MAX_LENGTH,
+	)} characters long`,
+	EMAIL_LOCAL_PART_MAX_LENGTH: `The local part of the email address must be at most ${String(
+		UserValidationRule.EMAIL_LOCAL_PART_MAX_LENGTH,
+	)} characters long`,
+	EMAIL_MAX_LENGTH: `Email must be at most ${String(
+		UserValidationRule.EMAIL_MAX_LENGTH,
+	)} characters long`,
 	EMAIL_REQUIRE: "Email is required",
 	EMAIL_TAKEN: "Email is taken",
 	EMAIL_WRONG: "Email is wrong",
+	NAME_INVALID_CHARACTERS:
+		"Please enter a valid name. Only letters, numbers, hyphens, and underscores are allowed",
+	NAME_MAX_LENGTH: `Name must be at most ${String(
+		UserValidationRule.NAME_MAX_LENGTH,
+	)} characters long`,
 	NAME_REQUIRE: "Name is required",
+	PASSWORD_INVALID_CHARACTERS:
+		"Password must include at least one letter and one number)",
 	PASSWORD_MAX_LENGTH: `Password must be at most ${String(
 		UserValidationRule.PASSWORD_MAX_LENGTH,
 	)} characters long`,
