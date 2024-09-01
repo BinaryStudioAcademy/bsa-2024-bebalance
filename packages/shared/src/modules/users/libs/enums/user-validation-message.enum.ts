57bf8934e7cc52094630d5a40afbd53e96cd3a16
Olha Bainurenova <77403180+Olha-Katran@users.noreply.github.com>
2024-09-01 12:28:17 +0300

    feat(mobile): requirements for authorization pages bb-210 (#259)
    * feat: change placeholder for auth fields bb-210
    
    * feat: update user-validation-message bb-210
    
    * feat: update user-validation-rule bb-210
    
    * feat: update user-sign-in-validation bb-210
    
    * feat: update user-sign-up-validation bb-210
    
    * feat: update user-validation for password bb-210
    
    * feat: update user-validation for email bb-210
    
    * feat: create enum regex bb-210
    
    * feat: update sign-in validation bb-210
    
    * feat: update sign-up validation bb-210

--- packages/shared/src/modules/users/libs/enums/user-validation-message.enum.ts
+++ packages/shared/src/modules/users/libs/enums/user-validation-message.enum.ts
@@ -1,10 +1,26 @@
 import { UserValidationRule } from "./user-validation-rule.enum.js";
 
 const UserValidationMessage = {
+	EMAIL_DOMAIN_PART_MAX_LENGTH: `The domain part of the email address must be at most ${String(
+		UserValidationRule.EMAIL_DOMAIN_PART_MAX_LENGTH,
+	)} characters long`,
+	EMAIL_LOCAL_PART_MAX_LENGTH: `The local part of the email address must be at most ${String(
+		UserValidationRule.EMAIL_LOCAL_PART_MAX_LENGTH,
+	)} characters long`,
+	EMAIL_MAX_LENGTH: `Email must be at most ${String(
+		UserValidationRule.EMAIL_MAX_LENGTH,
+	)} characters long`,
 	EMAIL_REQUIRE: "Email is required",
 	EMAIL_TAKEN: "Email is taken",
-	EMAIL_WRONG: "Email is wrong",
+	EMAIL_WRONG: "Please enter a valid email address",
+	NAME_INVALID_CHARACTERS:
+		"Please enter a valid name. Only letters, numbers, hyphens, and underscores are allowed",
+	NAME_MAX_LENGTH: `Name must be at most ${String(
+		UserValidationRule.NAME_MAX_LENGTH,
+	)} characters long`,
 	NAME_REQUIRE: "Name is required",
+	PASSWORD_INVALID_CHARACTERS:
+		"Your password contains invalid characters. Only letters, numbers, and ! @ # $ symbols are allowed",
 	PASSWORD_MAX_LENGTH: `Password must be at most ${String(
 		UserValidationRule.PASSWORD_MAX_LENGTH,
 	)} characters long`,
@@ -12,7 +28,8 @@ const UserValidationMessage = {
 		UserValidationRule.PASSWORD_MIN_LENGTH,
 	)} characters long`,
 	PASSWORD_REQUIRE: "Password is not allowed to be empty",
-	PASSWORD_SHORT: "Password length must be at least 8 characters long",
+	PASSWORD_REQUIRES_LETTER_AND_NUMBER:
+		"Password must include at least one letter and one number",
 } as const;
 
 export { UserValidationMessage };
