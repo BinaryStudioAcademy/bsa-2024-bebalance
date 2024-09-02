dcf6c90d247441424d5386a7de6ac212a763dcb7
Andrii Levin <andriilevin@gmail.com>
2024-09-02 19:21:48 +0100

    refactor(shared): correct the typo bb-134

--- packages/shared/src/modules/onboarding/libs/enums/enums.ts
+++ packages/shared/src/modules/onboarding/libs/enums/enums.ts
@@ -1,3 +1,3 @@
 export { OnboardingApiPath } from "./onboarding-api-path.enum.js";
 export { OnboardingValidationMessage } from "./onboarding-validation-message.enum.js";
-export { OnboardingValidationRule } from "./onboarding-validation-rule.emun.js";
+export { OnboardingValidationRule } from "./onboarding-validation-rule.enum.js";
