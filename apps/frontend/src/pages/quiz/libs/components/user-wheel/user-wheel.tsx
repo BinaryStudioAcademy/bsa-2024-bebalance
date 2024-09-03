edd0d6d2265668d4648e1b572e3913f6513c7d06
luiqor <vohnenna@gmail.com>
2024-09-03 12:01:40 +0300

    feat(frontend): update UserWheel component to use scores from quiz state bb-216

--- apps/frontend/src/pages/quiz/libs/components/user-wheel/user-wheel.tsx
+++ apps/frontend/src/pages/quiz/libs/components/user-wheel/user-wheel.tsx
@@ -1,28 +1,34 @@
 import { BalanceWheelChart } from "~/libs/components/components.js";
-import { useAppDispatch, useEffect } from "~/libs/hooks/hooks.js";
+import {
+	useAppDispatch,
+	useAppSelector,
+	useEffect,
+} from "~/libs/hooks/hooks.js";
 import { actions as quizActions } from "~/modules/quiz/quiz.js";
-
-const randomData = [
-	{ data: 10, label: "10" },
-	{ data: 9, label: "9" },
-	{ data: 8, label: "8" },
-	{ data: 6, label: "6" },
-	{ data: 5, label: "5" },
-	{ data: 4, label: "4" },
-	{ data: 3, label: "3" },
-	{ data: 1, label: "1" },
-];
 
 const UserWheel: React.FC = () => {
 	const dispatch = useAppDispatch();
+	const scores = useAppSelector((state) => state.quiz.scores);
+
 	useEffect(() => {
 		void dispatch(quizActions.getScores());
 	}, [dispatch]);
 
 	return (
-		<>
-			<BalanceWheelChart data={randomData} />
-		</>
+		<div>
+			<BalanceWheelChart
+				data={
+					scores
+						? scores.map((score) => {
+								return {
+									data: score.score,
+									label: String(score.score),
+								};
+							})
+						: []
+				}
+			/>
+		</div>
 	);
 };
 
