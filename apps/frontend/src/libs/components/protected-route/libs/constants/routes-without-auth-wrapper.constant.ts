6aea0273c4f1aa744386a8d8d6b801c7f177c350
ihorLysak <lysak.ihor4221@gmail.com>
2024-09-03 17:13:07 +0300

    refactor: changed typing on routes with auth wrapper constant bb-258

--- apps/frontend/src/libs/components/protected-route/libs/constants/routes-without-auth-wrapper.constant.ts
+++ apps/frontend/src/libs/components/protected-route/libs/constants/routes-without-auth-wrapper.constant.ts
@@ -1,8 +1,6 @@
 import { AppRoute } from "~/libs/enums/enums.js";
 import { type ValueOf } from "~/libs/types/types.js";
 
-const ROUTES_WITHOUT_AUTH_WRAPPER = [AppRoute.QUIZ] as ValueOf<
-	typeof AppRoute
->[];
+const ROUTES_WITHOUT_AUTH_WRAPPER: ValueOf<typeof AppRoute>[] = [AppRoute.QUIZ];
 
 export { ROUTES_WITHOUT_AUTH_WRAPPER };
