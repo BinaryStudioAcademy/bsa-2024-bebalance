5340ccbcd2c4c1f82e842c85184697f38f8fbf98
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-09-03 15:10:25 +0300

    refactor(mobile): moved categoriesForm the common components bb-250

--- apps/mobile/src/screens/chat/chat.tsx
+++ apps/mobile/src/screens/chat/chat.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-import { LoaderWrapper, ScreenWrapper } from "~/libs/components/components";
+import {
+	CategoriesForm,
+	LoaderWrapper,
+	ScreenWrapper,
+} from "~/libs/components/components";
 import { DataStatus } from "~/libs/enums/enums";
 import { useAppSelector } from "~/libs/hooks/hooks";
-
-import { CategoriesForm } from "./components/categories-form";
 
 const Chat: React.FC = () => {
 	const { dataStatus } = useAppSelector((state) => state.quiz);
