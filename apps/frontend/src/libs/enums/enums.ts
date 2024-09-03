6e35da79c022100b5ae58a35431fc473c153aeca
ihorLysak <lysak.ihor4221@gmail.com>
2024-09-03 19:04:42 +0300

    refactor: brought back expiration logic, added autherror handling to handle-error-middleware bb-258

--- apps/frontend/src/libs/enums/enums.ts
+++ apps/frontend/src/libs/enums/enums.ts
@@ -1,3 +1,9 @@
 export { AppRoute } from "./app-route.enum.js";
 export { DataStatus } from "./data-status.enum.js";
-export { APIPath, AppEnvironment, ContentType, ServerErrorType } from "shared";
+export {
+	APIPath,
+	AppEnvironment,
+	ContentType,
+	ErrorMessage,
+	ServerErrorType,
+} from "shared";
--- apps/frontend/src/libs/modules/store/handle-error.middleware.ts
+++ apps/frontend/src/libs/modules/store/handle-error.middleware.ts
@@ -1,12 +1,20 @@
 import { isRejected, type Middleware } from "@reduxjs/toolkit";
 
+import { ErrorMessage } from "~/libs/enums/enums.js";
 import { notification } from "~/libs/modules/notification/notification.js";
 
+import { storage, StorageKey } from "../storage/storage.js";
 import { type MiddlewareError } from "./libs/types/types.js";
 
 const handleErrorMiddleware: Middleware = () => {
-	return (next) => (action) => {
+	return (next) => async (action) => {
 		if (isRejected(action)) {
+			if (action.error.message === ErrorMessage.UNAUTHORIZED) {
+				await storage.drop(StorageKey.TOKEN);
+
+				return next(action);
+			}
+
 			const error = action.error as MiddlewareError;
 			notification.error(error.data ? error.data.message : error.message);
 		}
