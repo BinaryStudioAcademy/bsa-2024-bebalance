e132c448a2d3d08eabc84f1f906c587122b86bce
Jake Rosales <65630066+JD-Rosales@users.noreply.github.com>
2024-09-04 14:20:08 +0800

    fix(frontend): fix type error bb-204

--- apps/frontend/src/libs/types/types.ts
+++ apps/frontend/src/libs/types/types.ts
@@ -1,7 +1,4 @@
 export { type AsyncThunkConfig } from "./async-thunk-config.type.js";
-export { type IconName } from "./icon-name.type.js";
-export { type QueryParameters } from "./query-parameters.type.js";
-export { type SidebarItem } from "./sidebar-item.type.js";
 export {
 	type Control,
 	type FieldErrors,
