1177539f6c2e51affc621ab11a17d73ed6755259
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-09-04 21:17:15 +0300

    feat(frontend): add Checkbox component bb-243

---
+++ apps/frontend/src/libs/components/checkbox/checkbox.tsx
@@ -0,0 +1,73 @@
+import { useCallback, useFormController } from "~/libs/hooks/hooks.js";
+import {
+	type Control,
+	type FieldPath,
+	type FieldValues,
+	type InputOption,
+} from "~/libs/types/types.js";
+
+import styles from "./styles.module.css";
+
+type Properties<T extends FieldValues> = {
+	control: Control<T, null>;
+	label: string;
+	name: FieldPath<T>;
+	options?: InputOption[];
+};
+
+const Checkbox = <T extends FieldValues>({
+	control,
+	label,
+	name,
+	options = [],
+}: Properties<T>): JSX.Element => {
+	const { field } = useFormController({ control, name });
+	const fieldValue = field.value;
+
+	const handleCheckboxesChange = useCallback(
+		(event: React.ChangeEvent<HTMLInputElement>): void => {
+			const isChecked = event.target.checked;
+			const inputValue = event.target.value;
+
+			if (isChecked) {
+				field.onChange([...fieldValue, inputValue]);
+			} else {
+				field.onChange(
+					(fieldValue as string[]).filter((value) => value !== inputValue),
+				);
+			}
+		},
+		[fieldValue, field],
+	);
+
+	return (
+		<>
+			<label className={styles["input-wrapper"]}>
+				<p className={styles["question"]}>{label}</p>
+				<div className={styles["input-container"]}>
+					{options.map(({ label, value }) => {
+						return (
+							<div className={styles["gradient-border-container"]} key={value}>
+								<div className={styles["gradient-border-content"]}>
+									<label className={styles["label"]} key={value}>
+										<input
+											checked={(fieldValue as string[]).includes(value)}
+											className={styles["input"]}
+											onChange={handleCheckboxesChange}
+											type="checkbox"
+											value={value}
+										/>
+										<span className={styles["input-checkmark"]} />
+										{label}
+									</label>
+								</div>
+							</div>
+						);
+					})}
+				</div>
+			</label>
+		</>
+	);
+};
+
+export { Checkbox };
