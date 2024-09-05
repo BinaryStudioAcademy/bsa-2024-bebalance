ba67b84190caf30d3964935d83055a8152d0d62e
Olga <okatranis@gmail.com>
2024-09-05 20:02:56 +0300

    feat: add export bb-197

--- apps/mobile/src/libs/helpers/helpers.ts
+++ apps/mobile/src/libs/helpers/helpers.ts
@@ -1,3 +1,5 @@
 export { checkIfAndroid, checkIfIos } from "./check-platform/check-platform";
+export { generateGradientColors } from "./generate-slider-gradient/generate-slider-gradient-colors.helper";
+export { generateGradientLocations } from "./generate-slider-gradient/generate-slider-gradient-locations.helper";
 export { getSecurityInputIconName } from "./get-security-input-icon-name/get-security-input-icon-name.helper";
 export { configureString } from "shared";
