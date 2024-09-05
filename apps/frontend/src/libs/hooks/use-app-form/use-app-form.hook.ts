d06b88e550fb7acfb7619beb6a1017b783d49fd5
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-09-05 21:50:37 +0300

    feat(frontend): add watch to useAppForm bb-243

--- apps/frontend/src/libs/hooks/use-app-form/use-app-form.hook.ts
+++ apps/frontend/src/libs/hooks/use-app-form/use-app-form.hook.ts
@@ -8,6 +8,7 @@ import {
 	type UseFormHandleSubmit,
 	type UseFormProps,
 	type UseFormSetError,
+	type UseFormWatch,
 	type ValidationMode,
 } from "react-hook-form";
 import { useForm } from "react-hook-form";
@@ -28,6 +29,7 @@ type ReturnValue<T extends FieldValues = FieldValues> = {
 	isValid: boolean;
 	reset: (defaultValues?: DefaultValues<T>) => void;
 	setError: UseFormSetError<T>;
+	watch: UseFormWatch<T>;
 };
 
 const useAppForm = <T extends FieldValues = FieldValues>({
@@ -54,6 +56,7 @@ const useAppForm = <T extends FieldValues = FieldValues>({
 		handleSubmit,
 		reset,
 		setError,
+		watch,
 	} = useForm<T>(parameters);
 
 	return {
@@ -64,6 +67,7 @@ const useAppForm = <T extends FieldValues = FieldValues>({
 		isValid,
 		reset,
 		setError,
+		watch,
 	};
 };
 
