68b0392e99605d9608ee93c4f46c2f08928e8d19
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-09-06 00:28:27 +0300

    refactor(backend): use patchAndFetchById in User repo bb-243

--- apps/backend/src/modules/users/user.repository.ts
+++ apps/backend/src/modules/users/user.repository.ts
@@ -196,7 +196,7 @@ class UserRepository implements Repository {
 		id: number,
 		payload: Partial<UserDetailsModel>,
 	): Promise<null | UserEntity> {
-		await this.userDetailsModel.query().patch(payload).where({ userId: id });
+		await this.userDetailsModel.query().patchAndFetchById(id, payload);
 
 		const user = await this.userModel
 			.query()
