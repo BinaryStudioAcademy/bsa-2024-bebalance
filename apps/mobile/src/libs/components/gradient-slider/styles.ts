710ac5351a2f5838eb372e2abdbe3fa453d45a2c
Olga <okatranis@gmail.com>
2024-09-05 20:01:42 +0300

    feat: create gradient slider-content component, add style bb-197

---
+++ apps/mobile/src/libs/components/gradient-slider/styles.ts
@@ -0,0 +1,36 @@
+import { StyleSheet } from "react-native";
+
+import { BaseColor } from "~/libs/enums/enums";
+
+const styles = StyleSheet.create({
+	gradient: {
+		borderRadius: 20,
+		height: 6.5,
+		position: "relative",
+	},
+	labelContainer: {
+		bottom: 10,
+		position: "absolute",
+	},
+	labelText: {
+		backgroundColor: BaseColor.BLACK,
+		color: BaseColor.BG_WHITE,
+		paddingHorizontal: 1,
+		position: "absolute",
+	},
+	planet: {
+		position: "absolute",
+	},
+	slider: {
+		padding: 0,
+		position: "absolute",
+		width: "105%",
+	},
+	sliderContainer: {
+		borderRadius: 20,
+		position: "relative",
+		width: "90%",
+	},
+});
+
+export { styles };
