1917b1cd7974575b4a57cb973a8cc6fd06ca26e7
Olga <okatranis@gmail.com>
2024-09-05 20:06:07 +0300

    feat: add temp usage bb-197

--- apps/mobile/src/screens/settings/settings.tsx
+++ apps/mobile/src/screens/settings/settings.tsx
@@ -1,12 +1,52 @@
 import React from "react";
 
-import { ScreenWrapper, Text } from "~/libs/components/components";
+import {
+	BackgroundWrapper,
+	ScreenWrapper,
+	SliderContent,
+	View,
+} from "~/libs/components/components";
+import { type colorToGradientColors } from "~/libs/maps/maps";
+import { globalStyles } from "~/libs/styles/styles";
+
+import { styles } from "./styles";
+
+const sliderData: {
+	color: keyof typeof colorToGradientColors;
+	label: string;
+}[] = [
+	{ color: "red", label: "Love" },
+	{ color: "lime", label: "Work" },
+	{ color: "green", label: "Money" },
+	{ color: "violet", label: "Friends" },
+	{ color: "yellow", label: "Physical" },
+	{ color: "pink", label: "Free time" },
+	{ color: "orange", label: "Spiritual" },
+	{ color: "blue", label: "Mental" },
+];
 
 const Settings: React.FC = () => {
 	return (
-		<ScreenWrapper>
-			<Text>Settings!</Text>
-		</ScreenWrapper>
+		<BackgroundWrapper>
+			<ScreenWrapper>
+				<View
+					style={[
+						globalStyles.flex1,
+						globalStyles.justifyContentCenter,
+						globalStyles.mb16,
+						globalStyles.mh12,
+						globalStyles.mt32,
+						globalStyles.ph12,
+						globalStyles.pt48,
+						styles.container,
+					]}
+				>
+					{sliderData.map((item, index) => (
+						<SliderContent color={item.color} key={index} label={item.label} />
+					))}
+				</View>
+			</ScreenWrapper>
+		</BackgroundWrapper>
 	);
 };
 
