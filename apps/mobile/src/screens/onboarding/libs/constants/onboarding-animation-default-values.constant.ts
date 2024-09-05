a03f5c5d166280b3d224f294ef1c28fe1b503b0b
KotenkoNata <kotenkonata8@gmail.com>
2024-09-05 17:58:24 +0200

    fix(mobile): add uncommited files  bb-127

---
+++ apps/mobile/src/screens/onboarding/libs/constants/onboarding-animation-default-values.constant.ts
@@ -0,0 +1,7 @@
+const AnimationDefaultValues = {
+	MULTIPLIER: 0.5,
+	ONE: 1,
+	ZERO: 0,
+};
+
+export { AnimationDefaultValues };
--- apps/mobile/src/screens/onboarding/libs/constants/onboarding-animation-default-values.constants.ts
+++
@@ -1,5 +0,0 @@
-const ZERO = 0;
-const MULTIPLIER = 0.5;
-const ONE = 1;
-
-export { MULTIPLIER, ONE, ZERO };
