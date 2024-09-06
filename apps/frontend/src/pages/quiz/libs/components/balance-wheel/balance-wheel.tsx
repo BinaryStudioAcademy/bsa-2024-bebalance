3cfd748865173948133b2780fcc9f8ea06096eee
JD-Rosales <rosales.jake1999@gmail.com>
2024-09-06 17:12:15 +0800

    feat(frontend): added incrementing analyzing text bb-204

--- apps/frontend/src/pages/quiz/libs/components/balance-wheel/balance-wheel.tsx
+++ apps/frontend/src/pages/quiz/libs/components/balance-wheel/balance-wheel.tsx
@@ -1,10 +1,47 @@
 import { BalanceWheelChart } from "~/libs/components/components.js";
 import { getValidClassNames } from "~/libs/helpers/helpers.js";
+import { useCallback, useEffect, useState } from "~/libs/hooks/hooks.js";
 
-import { BALANCE_WHEEL_ANIMATED_INITIAL_DATA } from "./libs/constants/constants.js";
+import {
+	BALANCE_WHEEL_ANIMATED_INITIAL_DATA,
+	PERCENTAGE_INCREASE_INTERVAL,
+} from "./libs/constants/constants.js";
+import { PercentageConfig } from "./libs/enums/enums.js";
 import styles from "./styles.module.css";
 
 const BalanceWheel: React.FC = () => {
+	const [percentage, setPercentage] = useState<number>(
+		PercentageConfig.DEFAULT_VALUE,
+	);
+
+	const handleUpdatePercentage = useCallback(() => {
+		setPercentage((previousPercentage) => {
+			if (previousPercentage >= PercentageConfig.MAX_VALUE) {
+				return previousPercentage;
+			}
+
+			const randomIncrease =
+				Math.floor(Math.random() * PercentageConfig.MAX_INCREMENT) +
+				PercentageConfig.MIN_INCREMENT;
+			const newPercentage = previousPercentage + randomIncrease;
+
+			return newPercentage > PercentageConfig.MAX_VALUE
+				? PercentageConfig.MAX_VALUE
+				: newPercentage;
+		});
+	}, []);
+
+	useEffect(() => {
+		const intervalId = setInterval(
+			handleUpdatePercentage,
+			PERCENTAGE_INCREASE_INTERVAL,
+		);
+
+		return (): void => {
+			clearInterval(intervalId);
+		};
+	}, [handleUpdatePercentage]);
+
 	return (
 		<div className={styles["container"]}>
 			<div className={styles["border-container"]} />
@@ -32,6 +69,7 @@ const BalanceWheel: React.FC = () => {
 				data={BALANCE_WHEEL_ANIMATED_INITIAL_DATA}
 				isAnimating
 			/>
+			<span className={styles["text"]}>Analyzing {percentage}%</span>
 		</div>
 	);
 };
---
+++ apps/frontend/src/pages/quiz/libs/components/balance-wheel/libs/enums/percentage-config.enum.ts
@@ -0,0 +1,8 @@
+const PercentageConfig = {
+	DEFAULT_VALUE: 0,
+	MAX_INCREMENT: 10,
+	MAX_VALUE: 100,
+	MIN_INCREMENT: 1,
+} as const;
+
+export { PercentageConfig };
