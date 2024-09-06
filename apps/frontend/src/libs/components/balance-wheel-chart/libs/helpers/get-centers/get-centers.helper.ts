a0de157937dfe0444a1e9ceadd3f3d32de5f1054
luiqor <vohnenna@gmail.com>
2024-09-06 17:49:41 +0300

    refactor(frontend): update import paths for get center helper bb-216

--- apps/frontend/src/libs/components/balance-wheel-chart/libs/helpers/get-centers/get-centers.helper.ts
+++ apps/frontend/src/libs/components/balance-wheel-chart/libs/helpers/get-centers/get-centers.helper.ts
@@ -1,6 +1,6 @@
 import { type ChartArea } from "~/libs/types/types.js";
 
-import { type CentersCoordinates } from "../../types/center-coordinates.type.js";
+import { type CentersCoordinates } from "../../types/types.js";
 import { getAbsoluteCenter } from "../get-absolute-center/get-absolute-center.helper.js";
 import { getFirstPartCenter } from "../get-first-part-center/get-first-part-center.helper.js";
 import { getSecondPartCenter } from "../get-second-part-center/get-second-part-center.helper.js";
