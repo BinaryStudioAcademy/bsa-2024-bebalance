63d10b6016283d5797bce8e4a189a366aad11512
JD-Rosales <rosales.jake1999@gmail.com>
2024-09-06 16:50:20 +0800

    feat(frontend): added circle gradient bb-204

--- apps/frontend/src/pages/quiz/libs/components/balance-wheel/balance-wheel.tsx
+++ apps/frontend/src/pages/quiz/libs/components/balance-wheel/balance-wheel.tsx
@@ -1,4 +1,5 @@
 import { BalanceWheelChart } from "~/libs/components/components.js";
+import { getValidClassNames } from "~/libs/helpers/helpers.js";
 
 import { BALANCE_WHEEL_ANIMATED_INITIAL_DATA } from "./libs/constants/constants.js";
 import styles from "./styles.module.css";
@@ -6,12 +7,31 @@ import styles from "./styles.module.css";
 const BalanceWheel: React.FC = () => {
 	return (
 		<div className={styles["container"]}>
+			<div className={styles["border-container"]} />
+			<div className={styles["white-dots"]} />
+			<div
+				className={getValidClassNames(
+					styles["circle-gradient"],
+					styles["circle-1"],
+				)}
+			/>
+			<div
+				className={getValidClassNames(
+					styles["circle-gradient"],
+					styles["circle-2"],
+				)}
+			/>
+			<div
+				className={getValidClassNames(
+					styles["circle-gradient"],
+					styles["circle-3"],
+				)}
+			/>
+
 			<BalanceWheelChart
 				data={BALANCE_WHEEL_ANIMATED_INITIAL_DATA}
 				isAnimating
 			/>
-
-			<div className={styles["white-dots"]} />
 		</div>
 	);
 };
