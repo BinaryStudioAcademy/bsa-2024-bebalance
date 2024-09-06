b8305f3fd2fcb82efa1e027a304ea5fbf751aac5
KeidsID <keidscode@gmail.com>
2024-09-06 20:00:16 +0800

    feat(frontend): update categories form on the `Chat` page bb-220

--- apps/frontend/src/pages/chat/chat.tsx
+++ apps/frontend/src/pages/chat/chat.tsx
@@ -1,9 +1,17 @@
 import { QuizCategoriesForm } from "~/libs/components/components.js";
 
+import styles from "./styles.module.css";
+
 const Chat: React.FC = () => {
 	return (
-		<main>
-			<QuizCategoriesForm />
+		<main className={styles["page-container"]}>
+			<section>Wheel Component</section>
+			<section className={styles["quiz-form-container"]}>
+				<strong>Do you feel any changes in anything?</strong>
+				<strong>Choose the fields you want to reestimate</strong>
+				<br />
+				<QuizCategoriesForm />
+			</section>
 		</main>
 	);
 };
