b3e4a0980926c1bc27e01f580a391b2749765c2d
KotenkoNata <kotenkonata8@gmail.com>
2024-09-07 16:35:19 +0200

    fix(mobile): replace types for RadioGroupValue bb-127

--- apps/mobile/src/screens/onboarding/libs/types/types.ts
+++ apps/mobile/src/screens/onboarding/libs/types/types.ts
@@ -1,5 +1,4 @@
 export { type OnboardingFormValues } from "./onboarding-form-values.type";
-export { type RadioGroupValue } from "./radio-group-value.type";
 export {
 	type OnboardingAnswerDto,
 	type OnboardingAnswerRequestBodyDto,
