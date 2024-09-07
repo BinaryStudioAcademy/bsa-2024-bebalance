ea27b94fc8ff45ae20667a858c9638340b24b468
Tetiana Brezhynska <brezhynska9012@gmail.com>
2024-09-07 14:55:38 +0200

    refactor(mobile): changed GRAIENT_SECTORS_DATA items scores to feat default wheel design bb-265

--- apps/mobile/src/libs/constants/gradient-sectors-data.constant.ts
+++ apps/mobile/src/libs/constants/gradient-sectors-data.constant.ts
@@ -2,13 +2,13 @@ import { GradientColor } from "../enums/enums";
 import { type WheelDataItem } from "../types/types";
 
 const GRADIENT_SECTORS_DATA: WheelDataItem[] = [
-	{ colors: GradientColor.YELLOW, label: "Physical", score: 8 },
-	{ colors: GradientColor.LIME, label: "Work", score: 5 },
-	{ colors: GradientColor.VIOLET, label: "Friends", score: 6 },
-	{ colors: GradientColor.RED, label: "Love", score: 4 },
-	{ colors: GradientColor.GREEN, label: "Money", score: 10 },
-	{ colors: GradientColor.ROSE, label: "Free time", score: 9 },
-	{ colors: GradientColor.ORANGE, label: "Spiritual", score: 6 },
+	{ colors: GradientColor.YELLOW, label: "Physical", score: 9 },
+	{ colors: GradientColor.LIME, label: "Work", score: 8 },
+	{ colors: GradientColor.VIOLET, label: "Friends", score: 7 },
+	{ colors: GradientColor.RED, label: "Love", score: 6 },
+	{ colors: GradientColor.GREEN, label: "Money", score: 5 },
+	{ colors: GradientColor.ROSE, label: "Free time", score: 8 },
+	{ colors: GradientColor.ORANGE, label: "Spiritual", score: 5 },
 	{ colors: GradientColor.BLUE, label: "Mental", score: 7 },
 ];
 
