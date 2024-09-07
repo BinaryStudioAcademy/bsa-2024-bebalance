8cb0161c55757bcb5a41882374558f021a3d4dbe
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-09-08 00:36:06 +0300

    fix(shared): import path bb-243

--- packages/shared/src/modules/users/libs/types/notification-answers-payload-dto.type.ts
+++ packages/shared/src/modules/users/libs/types/notification-answers-payload-dto.type.ts
@@ -1,5 +1,5 @@
 import { type ValueOf } from "../../../../libs/types/types.js";
-import { type NotificationFrequency } from "../enums/notification-frequency.enum.js";
+import { type NotificationFrequency } from "../enums/enums.js";
 
 type NotificationAnswersPayloadDto = {
 	notificationFrequency: ValueOf<typeof NotificationFrequency>;
