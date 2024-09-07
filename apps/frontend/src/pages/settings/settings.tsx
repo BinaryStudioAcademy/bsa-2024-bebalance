c22134a9b143d908e335ac9059f4221395b3d5b3
ihorLysak <lysak.ihor4221@gmail.com>
2024-09-07 20:16:47 +0300

    feat: added settings to sidebar bb-334

---
+++ apps/frontend/src/pages/settings/settings.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+const Settings: React.FC = () => {
+	return (
+		<div className={styles["container"]}>
+			<h1>Settings</h1>
+		</div>
+	);
+};
+
+export { Settings };
