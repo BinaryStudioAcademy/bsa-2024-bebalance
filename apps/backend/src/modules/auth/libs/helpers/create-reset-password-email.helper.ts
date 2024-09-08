7c75a91a8e39a3b696e770a4089c9c4c26dbd6cb
ihorLysak <lysak.ihor4221@gmail.com>
2024-09-08 15:18:46 +0300

    refactor: changed text created by createResetPasswordEmail helper bb-285

--- apps/backend/src/modules/auth/libs/helpers/create-reset-password-email.helper.ts
+++ apps/backend/src/modules/auth/libs/helpers/create-reset-password-email.helper.ts
@@ -8,7 +8,7 @@ function createResetPasswordEmail({
 	return `
 <p>We’ve received a request to reset your password.</p>
 <p>Dear ${username}, please follow the link to set up a new password: <a href="${link}">click here to reset your password</a></p>
-<p><strong>The link expires in 30 minutes for security purposes.</strong></p>
+<p><strong>The link expires in 30 minutes.</strong></p>
 <p>If you have not requested a password reset, kindly ignore this email.</p>
 `;
 }
