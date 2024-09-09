cc3269954a87fb17ed19c7aed11265e6d288326b
Max Korop <makskorop99@gmail.com>
2024-09-09 11:31:59 +0300

    fix(backend): add index.d.ts bb-173

--- apps/backend/index.d.ts
+++ apps/backend/index.d.ts
@@ -1,8 +1,11 @@
 import { type MultipartFile } from "@fastify/multipart";
 import "fastify";
 
+import { type UserDto } from "~/libs/types/types.js";
+
 declare module "fastify" {
 	interface FastifyRequest {
 		uploadedFile?: MultipartFile;
+		user?: UserDto;
 	}
 }
