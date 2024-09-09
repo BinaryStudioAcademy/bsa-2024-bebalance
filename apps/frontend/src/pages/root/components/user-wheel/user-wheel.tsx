500090ca565a49a55865b9166b8515b24228f935
luiqor <vohnenna@gmail.com>
2024-09-09 08:27:07 +0300

    feat(frontend): add handleEditing onclick action for UserWheel bb-309

--- apps/frontend/src/pages/root/components/user-wheel/user-wheel.tsx
+++ apps/frontend/src/pages/root/components/user-wheel/user-wheel.tsx
@@ -2,7 +2,9 @@ import { BalanceWheelChart, Button } from "~/libs/components/components.js";
 import {
 	useAppDispatch,
 	useAppSelector,
+	useCallback,
 	useEffect,
+	useState,
 } from "~/libs/hooks/hooks.js";
 import { actions as quizActions } from "~/modules/quiz/quiz.js";
 
@@ -12,6 +14,12 @@ const UserWheel: React.FC = () => {
 	const NO_SCORES_COUNT = 0;
 	const dispatch = useAppDispatch();
 	const scores = useAppSelector((state) => state.quiz.scores);
+
+	const [isEditingModalOpen, setIsEditingModalOpen] = useState<boolean>(false);
+
+	const handleEditing = useCallback(() => {
+		setIsEditingModalOpen(true);
+	}, []);
 
 	useEffect(() => {
 		void dispatch(quizActions.getScores());
@@ -24,17 +32,21 @@ const UserWheel: React.FC = () => {
 		};
 	});
 
+	const headerText = isEditingModalOpen ? "Edit my wheel" : "My wheel results";
+
 	return (
 		<div className={styles["container"]}>
 			<div className={styles["header"]}>
-				<h4 className={styles["header-text"]}>My wheel results</h4>
+				<h4 className={styles["header-text"]}>{headerText}</h4>
 			</div>
 			<div className={styles["chart-wrapper"]}>
 				{scores.length > NO_SCORES_COUNT && (
 					<BalanceWheelChart data={chartData} />
 				)}
 			</div>
-			<Button label="edit my wheel results" />
+			{isEditingModalOpen || (
+				<Button label="edit my wheel results" onClick={handleEditing} />
+			)}
 		</div>
 	);
 };
