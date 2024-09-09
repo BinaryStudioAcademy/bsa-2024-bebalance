27b29fe752450d749d5e2103a567e6dc9921fd81
Andrii Levin <andriilevin@gmail.com>
2024-09-09 21:54:01 +0100

    feat(backend): add init and balance prompts enum bb-203

---
+++ apps/backend/src/libs/modules/open-ai/libs/enums/enums.ts
@@ -0,0 +1,4 @@
+export { OpenAiAssistantConfig } from "./open-ai-assistant-config.enum.js";
+export { OpenAIErrorMessage } from "./open-ai-error-message.enum.js";
+export { OpenAiPromptTemplates } from "./open-ai-promt-mesagges.enum.js";
+export { OpenAiRoleKey } from "./open-ai-role-key.enum.js";
