9963790a13f0f772cbcbcd4a5ca41ced33c4ea67
Andrii Levin <andriilevin@gmail.com>
2024-09-09 20:30:55 +0100

    chore(backend): add openai types   bb-203

---
+++ apps/backend/src/libs/modules/open-ai/libs/types/types.ts
@@ -0,0 +1,2 @@
+export { type MessageCreateParams as OpenAiRequestMessage } from "openai/resources/beta/threads/index";
+export { type Message as OpenAiResponseMessage } from "openai/resources/beta/threads/index";
