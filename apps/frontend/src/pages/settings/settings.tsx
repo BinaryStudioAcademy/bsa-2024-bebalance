4148fe16e58df039c537ac23d0d1434e49bc7172
ihorLysak <lysak.ihor4221@gmail.com>
2024-09-10 11:16:41 +0300

    feat: added submit form function to settings bb-334

--- apps/frontend/src/pages/settings/settings.tsx
+++ apps/frontend/src/pages/settings/settings.tsx
@@ -1,9 +1,16 @@
 import { Button, Checkbox, Input } from "~/libs/components/components.js";
-import { useAppForm, useAppSelector } from "~/libs/hooks/hooks.js";
+import {
+	useAppDispatch,
+	useAppForm,
+	useAppSelector,
+	useCallback,
+} from "~/libs/hooks/hooks.js";
 import { type ValueOf } from "~/libs/types/types.js";
 import {
+	type NotificationAnswersPayloadDto,
 	notificationAnswersValidationSchema,
 	type NotificationFrequency,
+	actions as userActions,
 	type UserDto,
 } from "~/modules/users/users.js";
 import {
@@ -16,18 +23,38 @@ import styles from "./styles.module.css";
 
 const Settings: React.FC = () => {
 	const user = useAppSelector((state) => state.auth.user) as UserDto;
-	const { control } = useAppForm<NotificationQuestionsFormValues>({
-		defaultValues: {
-			notificationFrequency: user.notificationFrequency as ValueOf<
-				typeof NotificationFrequency
-			>,
-			userTaskDays: user.userTaskDays as number[],
+	const { control, handleSubmit } = useAppForm<NotificationQuestionsFormValues>(
+		{
+			defaultValues: {
+				notificationFrequency: user.notificationFrequency as ValueOf<
+					typeof NotificationFrequency
+				>,
+				userTaskDays: user.userTaskDays as number[],
+			},
+			validationSchema: notificationAnswersValidationSchema,
 		},
-		validationSchema: notificationAnswersValidationSchema,
-	});
+	);
+
+	const dispatch = useAppDispatch();
+
+	const handleNotificationQuestionsSubmit = useCallback(
+		(payload: NotificationAnswersPayloadDto): void => {
+			void dispatch(userActions.saveNotificationAnswers(payload));
+		},
+		[dispatch],
+	);
+
+	const handleFormSubmit = useCallback(
+		(event_: React.BaseSyntheticEvent): void => {
+			void handleSubmit((payload) => {
+				handleNotificationQuestionsSubmit(payload);
+			})(event_);
+		},
+		[handleNotificationQuestionsSubmit, handleSubmit],
+	);
 
 	return (
-		<form className={styles["container"]}>
+		<form className={styles["container"]} onSubmit={handleFormSubmit}>
 			<div className={styles["form-header"]}>
 				<h1>Settings</h1>
 				<Button label="save changes" type="submit" />
