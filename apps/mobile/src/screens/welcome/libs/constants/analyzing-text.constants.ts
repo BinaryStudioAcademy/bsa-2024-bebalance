e7e94f59d1ec17db20fada69a0f978d62b355b8b
Olga <okatranis@gmail.com>
2024-09-10 23:12:18 +0300

    feat: add analyzing text bb-344

---
+++ apps/mobile/src/screens/welcome/libs/constants/analyzing-text.constants.ts
@@ -0,0 +1,7 @@
+const AnalyzingText =
+	"Thank you for sharing your insights! We’re currently processing your responses to create a personalized path just for you." +
+	" This is where the magic begins—we’re using your input to tailor the experience, offering you the guidance and motivation you need to achieve a balanced," +
+	" fulfilling life. \n\n Hang tight while we set things up! In just a moment, you’ll dive into the areas that matter most to you, and together, " +
+	"we’ll start making progress toward your goals. Your journey to a better life starts now!";
+
+export { AnalyzingText };
---
+++ apps/mobile/src/screens/welcome/libs/constants/constants.ts
@@ -0,0 +1,1 @@
+export { AnalyzingText } from "./analyzing-text.constants";
