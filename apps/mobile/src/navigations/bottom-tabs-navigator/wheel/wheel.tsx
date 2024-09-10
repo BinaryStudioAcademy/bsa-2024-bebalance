f7e9af57b2a743908b7da1a21495bdae4940db3d
Olga <okatranis@gmail.com>
2024-09-10 12:43:47 +0300

    feat: create wheel-nav with 2 screen - wheel and edit bb-344

---
+++ apps/mobile/src/navigations/bottom-tabs-navigator/wheel/wheel.tsx
@@ -0,0 +1,24 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+
+import { BottomTabScreenName } from "~/libs/enums/enums";
+import { type BottomTabNavigationParameterList } from "~/libs/types/types";
+import { EditWheelResults } from "~/screens/edit-wheel-results/edit-wheel-results";
+import { Wheel } from "~/screens/wheel/wheel";
+
+const WheelStack =
+	createNativeStackNavigator<BottomTabNavigationParameterList>();
+
+const WheelStackNavigator: React.FC = () => {
+	return (
+		<WheelStack.Navigator screenOptions={{ headerShown: false }}>
+			<WheelStack.Screen component={Wheel} name={BottomTabScreenName.WHEEL} />
+			<WheelStack.Screen
+				component={EditWheelResults}
+				name={BottomTabScreenName.EDIT_WHEEL_RESULTS}
+			/>
+		</WheelStack.Navigator>
+	);
+};
+
+export { WheelStackNavigator };
