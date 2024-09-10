7cb285fb60887ec143efea8fefdc0954c778d586
ihorLysak <lysak.ihor4221@gmail.com>
2024-09-10 14:20:21 +0300

    refactor: rewritten handleErrorMiddleware to be a single function bb-285

--- apps/frontend/src/libs/modules/store/libs/middlewares/handle-error.middleware.ts
+++ apps/frontend/src/libs/modules/store/libs/middlewares/handle-error.middleware.ts
@@ -1,59 +1,43 @@
 import { isRejected, type Middleware } from "@reduxjs/toolkit";
 
-import { AppRoute, ErrorMessage } from "~/libs/enums/enums.js";
+import { ErrorMessage } from "~/libs/enums/enums.js";
 import { StorageKey } from "~/libs/modules/storage/storage.js";
-import { store } from "~/libs/modules/store/store.js";
-import { actions as appActions } from "~/modules/app/app.js";
 
 import { type ExtraArguments } from "../../store.js";
 import { type MiddlewareError } from "../types/types.js";
-
-type Properties = {
-	action: unknown;
-	extra: ExtraArguments;
-	next: (action: unknown) => unknown;
-};
-
-const handleErrorMiddlewareExecution = async ({
-	action,
-	extra: { notification, storage },
-	next,
-}: Properties): Promise<unknown> => {
-	if (isRejected(action)) {
-		switch (action.error.message) {
-			case ErrorMessage.UNAUTHORIZED: {
-				await storage.drop(StorageKey.TOKEN);
-				store.instance.dispatch(appActions.changeLink(AppRoute.SIGN_IN));
-
-				return next(action);
-			}
-
-			case ErrorMessage.RESET_PASSWORD_LINK_EXPIRED: {
-				notification.error(ErrorMessage.RESET_PASSWORD_LINK_EXPIRED);
-				store.instance.dispatch(appActions.changeLink(AppRoute.SIGN_IN));
-
-				return next(action);
-			}
-
-			default: {
-				const error = action.error as MiddlewareError;
-				notification.error(error.data ? error.data.message : error.message);
-				await storage.drop(StorageKey.TOKEN);
-				store.instance.dispatch(appActions.changeLink(AppRoute.SIGN_IN));
-
-				return next(action);
-			}
-		}
-	}
-
-	return next(action);
-};
 
 const handleErrorMiddleware = (extra: ExtraArguments): Middleware => {
 	return () => {
 		return (next) => {
 			return async (action) => {
-				await handleErrorMiddlewareExecution({ action, extra, next });
+				if (isRejected(action)) {
+					switch (action.error.message) {
+						case ErrorMessage.UNAUTHORIZED: {
+							await extra.storage.drop(StorageKey.TOKEN);
+
+							return next(action);
+						}
+
+						case ErrorMessage.RESET_PASSWORD_LINK_EXPIRED: {
+							extra.notification.error(
+								ErrorMessage.RESET_PASSWORD_LINK_EXPIRED,
+							);
+
+							return next(action);
+						}
+
+						default: {
+							const error = action.error as MiddlewareError;
+							extra.notification.error(
+								error.data ? error.data.message : error.message,
+							);
+
+							return next(action);
+						}
+					}
+				}
+
+				return next(action);
 			};
 		};
 	};
