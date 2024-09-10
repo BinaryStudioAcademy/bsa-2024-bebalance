81486fdc3e0f123313cd29a68c9d5b0ea547906a
ihorLysak <lysak.ihor4221@gmail.com>
2024-09-10 17:20:28 +0300

    refactor: destructured services call inside of handle error middleware bb-285

--- apps/frontend/src/libs/modules/store/libs/middlewares/handle-error.middleware.ts
+++ apps/frontend/src/libs/modules/store/libs/middlewares/handle-error.middleware.ts
@@ -6,29 +6,30 @@ import { StorageKey } from "~/libs/modules/storage/storage.js";
 import { type ExtraArguments } from "../../store.js";
 import { type MiddlewareError } from "../types/types.js";
 
-const handleErrorMiddleware = (extra: ExtraArguments): Middleware => {
+const handleErrorMiddleware = ({
+	notification,
+	storage,
+}: ExtraArguments): Middleware => {
 	return () => {
 		return (next) => {
 			return async (action) => {
 				if (isRejected(action)) {
 					switch (action.error.message) {
 						case ErrorMessage.UNAUTHORIZED: {
-							await extra.storage.drop(StorageKey.TOKEN);
+							await storage.drop(StorageKey.TOKEN);
 
 							return next(action);
 						}
 
 						case ErrorMessage.RESET_PASSWORD_LINK_EXPIRED: {
-							extra.notification.error(
-								ErrorMessage.RESET_PASSWORD_LINK_EXPIRED,
-							);
+							notification.error(ErrorMessage.RESET_PASSWORD_LINK_EXPIRED);
 
 							return next(action);
 						}
 
 						default: {
 							const error = action.error as MiddlewareError;
-							extra.notification.error(
+							notification.error(
 								error.data ? error.data.message : error.message,
 							);
 
