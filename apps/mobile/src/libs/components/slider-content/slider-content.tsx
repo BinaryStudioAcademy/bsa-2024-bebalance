51a2d923b639cf2f65e6a1896f91476bfcaf42d2
Olga <okatranis@gmail.com>
2024-09-10 14:53:01 +0300

    feat: add init-value to type slider-data bb-344

--- apps/mobile/src/libs/components/slider-content/slider-content.tsx
+++ apps/mobile/src/libs/components/slider-content/slider-content.tsx
@@ -6,27 +6,30 @@ import { globalStyles } from "~/libs/styles/styles";
 
 type Properties = {
 	color: keyof typeof colorToGradientColors;
+	initValue: number;
 	label: string;
 };
 
 const MAX_SLIDER_VALUE = 10;
 const MIN_SLIDER_VALUE = 0;
 
-const SliderContent: React.FC<Properties> = ({ color, label }) => {
+const SliderContent: React.FC<Properties> = ({ color, initValue, label }) => {
 	return (
 		<View
 			style={[
 				globalStyles.flex1,
 				globalStyles.justifyContentCenter,
-				globalStyles.mt48,
+				globalStyles.mt32,
 				globalStyles.mh32,
+				globalStyles.pt8,
 			]}
 		>
-			<Text preset="tag" style={globalStyles.mb48}>
+			<Text preset="tag" style={globalStyles.mb4}>
 				{label}
 			</Text>
 			<GradientSlider
 				gradientColors={color}
+				initValue={initValue}
 				max={MAX_SLIDER_VALUE}
 				min={MIN_SLIDER_VALUE}
 			/>
--- apps/mobile/src/libs/types/slider/slider.ts
+++ apps/mobile/src/libs/types/slider/slider.ts
@@ -2,6 +2,7 @@ import { type colorToGradientColors } from "~/libs/maps/maps";
 
 type SliderData = {
 	color: keyof typeof colorToGradientColors;
+	initValue: number;
 	label: string;
 };
 
