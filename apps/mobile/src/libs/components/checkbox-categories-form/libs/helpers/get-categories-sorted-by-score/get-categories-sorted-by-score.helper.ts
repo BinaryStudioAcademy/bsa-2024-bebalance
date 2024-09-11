c47ac4f3183334c0f9d6f81ae21b5a180491058a
Tetiana Brezhynska <brezhynska9012@gmail.com>
2024-09-11 19:06:11 +0200

    refactor(mobile): simplified sorting logic in getCategoriesSortedByScore helper  bb-250

--- apps/mobile/src/libs/components/checkbox-categories-form/libs/helpers/get-categories-sorted-by-score/get-categories-sorted-by-score.helper.ts
+++ apps/mobile/src/libs/components/checkbox-categories-form/libs/helpers/get-categories-sorted-by-score/get-categories-sorted-by-score.helper.ts
@@ -1,25 +1,11 @@
 import { type QuizScoresGetAllItemResponseDto } from "~/libs/types/types";
 
-const PLACE_AFTER_VALUE = 1;
-const PLACE_BEFORE_VALUE = -1;
-const STAY_IN_PLACE_VALUE = 0;
-
 const getCategoriesSortedByScore = (
 	categoriesData: QuizScoresGetAllItemResponseDto[],
 ): QuizScoresGetAllItemResponseDto[] => {
 	return categoriesData
 		.map((categoryItem) => ({ ...categoryItem }))
-		.sort((a, b) => {
-			if (a.score < b.score) {
-				return PLACE_BEFORE_VALUE;
-			}
-
-			if (a.score > b.score) {
-				return PLACE_AFTER_VALUE;
-			}
-
-			return STAY_IN_PLACE_VALUE;
-		});
+		.sort((a, b) => a.score - b.score);
 };
 
 export { getCategoriesSortedByScore };
