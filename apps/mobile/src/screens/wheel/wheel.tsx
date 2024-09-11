407e74fd82d85b74a915a64cfab9a162658fd0ab
Olga <okatranis@gmail.com>
2024-09-11 19:18:35 +0300

    feat: delete unnecessary white-background-wrapper to prop style in screen-wrapper bb-344

--- apps/mobile/src/screens/wheel/wheel.tsx
+++ apps/mobile/src/screens/wheel/wheel.tsx
@@ -7,9 +7,8 @@ import {
 	Text,
 	View,
 	Wheel as WheelChart,
-	WhiteBackgroundWrapper,
 } from "~/libs/components/components";
-import { BottomTabScreenName } from "~/libs/enums/enums";
+import { BaseColor, BottomTabScreenName } from "~/libs/enums/enums";
 import { useCallback, useNavigation } from "~/libs/hooks/hooks";
 import { globalStyles } from "~/libs/styles/styles";
 import { type BottomTabNavigationParameterList } from "~/libs/types/types";
@@ -27,29 +26,27 @@ const Wheel: React.FC = () => {
 	}, [navigation]);
 
 	return (
-		<WhiteBackgroundWrapper>
-			<ScreenWrapper>
-				<View style={[globalStyles.m16]}>
-					<Text preset="subheading" weight="bold">
-						My Wheel Results
-					</Text>
-					<Text
-						preset="tag"
-						style={[globalStyles.pv4, globalStyles.ph12, styles.date]}
-					>
-						5 Aug 2024, Tuesday
-					</Text>
-					<View style={styles.container}>
-						<View style={globalStyles.alignItemsCenter}>
-							<WheelChart size={WHEEL_SIZE} />
-						</View>
-						<View style={[globalStyles.mh48]}>
-							<Button label="EDIT MY WHEEL RESULTS" onPress={handleEditPress} />
-						</View>
+		<ScreenWrapper style={{ backgroundColor: BaseColor.BG_WHITE }}>
+			<View style={[globalStyles.m16]}>
+				<Text preset="subheading" weight="bold">
+					My Wheel Results
+				</Text>
+				<Text
+					preset="tag"
+					style={[globalStyles.pv4, globalStyles.ph12, styles.date]}
+				>
+					5 Aug 2024, Tuesday
+				</Text>
+				<View style={styles.container}>
+					<View style={globalStyles.alignItemsCenter}>
+						<WheelChart size={WHEEL_SIZE} />
+					</View>
+					<View style={[globalStyles.mh48]}>
+						<Button label="EDIT MY WHEEL RESULTS" onPress={handleEditPress} />
 					</View>
 				</View>
-			</ScreenWrapper>
-		</WhiteBackgroundWrapper>
+			</View>
+		</ScreenWrapper>
 	);
 };
 
