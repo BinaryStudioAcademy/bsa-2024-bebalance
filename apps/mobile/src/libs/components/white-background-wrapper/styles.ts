b9961ecc3975c3f541e0d9e3a7d7fb96dd756a1d
Olga <okatranis@gmail.com>
2024-09-11 16:32:38 +0300

    feat: update wheel page, + style bb-344

--- apps/mobile/src/libs/components/white-background-wrapper/styles.ts
+++ apps/mobile/src/libs/components/white-background-wrapper/styles.ts
@@ -3,15 +3,8 @@ import { StyleSheet } from "react-native";
 import { BaseColor } from "~/libs/enums/enums";
 
 const styles = StyleSheet.create({
-	checkboxForm: {
-		borderColor: BaseColor.LIGHT_GRAY,
-		borderRadius: 14,
-		borderWidth: 1,
-		height: 500,
-	},
 	container: {
-		marginBottom: 0,
-		paddingBottom: 20,
+		backgroundColor: BaseColor.BG_WHITE,
 	},
 });
 
