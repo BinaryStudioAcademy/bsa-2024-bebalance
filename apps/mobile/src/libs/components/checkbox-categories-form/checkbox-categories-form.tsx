51a35e2c84088b424bc2eb131ce059b7065effaa
Tetiana Brezhynska <brezhynska9012@gmail.com>
2024-09-11 19:00:09 +0200

    refactor(mobile): moved update categories receiving logic to Chat screen  bb-250

--- apps/mobile/src/libs/components/checkbox-categories-form/checkbox-categories-form.tsx
+++ apps/mobile/src/libs/components/checkbox-categories-form/checkbox-categories-form.tsx
@@ -1,70 +1,48 @@
+import { Button, Checkbox, Text, View } from "~/libs/components/components";
+import { BaseColor, NumericalValue } from "~/libs/enums/enums";
 import {
-	Button,
-	Checkbox,
-	LoaderWrapper,
-	Text,
-	View,
-} from "~/libs/components/components";
-import { BaseColor, DataStatus } from "~/libs/enums/enums";
-import {
-	useAppDispatch,
 	useAppForm,
-	useAppSelector,
 	useCallback,
 	useEffect,
 	useFormController,
-	useState,
 } from "~/libs/hooks/hooks";
 import { globalStyles } from "~/libs/styles/styles";
+import {
+	type CategoriesSelectedRequestDto,
+	type QuizScoresGetAllItemResponseDto,
+} from "~/libs/types/types";
 import { categoriesSelectedValidationSchema } from "~/packages/categories/categories";
-import { actions as categoriesActions } from "~/slices/quiz/quiz";
 
 import {
 	CATEGORIES_FIELD_NAME,
 	CATEGORIES_FORM_DEFAULT_VALUES,
 	CHECK_ALL_CATEGORIES_NAME,
-	EMPTY_ARRAY_LENGTH,
 } from "./libs/constants/constants";
 import {
 	getAllCategoryIds,
 	getCategoriesSortedByScore,
 	getSelectedCategoryIds,
 } from "./libs/helpers/helpers";
-import { type SelectedCategoryIds } from "./libs/types/types";
 
 type Properties = {
-	onSubmit?: (payload: { categoryIds: SelectedCategoryIds }) => void;
+	categories: QuizScoresGetAllItemResponseDto[];
+	onSubmit: (payload: CategoriesSelectedRequestDto) => void;
 	submitButtonName: string;
 };
 
 const CheckboxCategoriesForm: React.FC<Properties> = ({
+	categories,
+	onSubmit,
 	submitButtonName,
-}: Properties): JSX.Element => {
-	const [submittedCategoryIds, setSubmittedCategoryIds] =
-		useState<SelectedCategoryIds>(CATEGORIES_FORM_DEFAULT_VALUES);
-
-	const dispatch = useAppDispatch();
-	const { categories, dataStatus } = useAppSelector((state) => state.quiz);
-
+}: Properties): JSX.Element | null => {
 	const { control, errors, handleSubmit, reset, setValue } = useAppForm<{
-		[CATEGORIES_FIELD_NAME]: SelectedCategoryIds;
+		[CATEGORIES_FIELD_NAME]: number[];
 	}>({
 		defaultValues: {
 			[CATEGORIES_FIELD_NAME]: CATEGORIES_FORM_DEFAULT_VALUES,
 		},
 		validationSchema: categoriesSelectedValidationSchema,
 	});
-
-	const { field } = useFormController({ control, name: CATEGORIES_FIELD_NAME });
-	const { value } = field;
-
-	const isFormLoading = dataStatus === DataStatus.PENDING;
-	const isAllChecked = value.length === categories.length;
-	const areCategoriesExist = categories.length > EMPTY_ARRAY_LENGTH;
-
-	useEffect(() => {
-		void dispatch(categoriesActions.getScores());
-	}, [dispatch]);
 
 	useEffect(() => {
 		reset({
@@ -74,16 +52,14 @@ const CheckboxCategoriesForm: React.FC<Properties> = ({
 		});
 	}, [reset, categories]);
 
+	const { field } = useFormController({ control, name: CATEGORIES_FIELD_NAME });
+	const { value } = field;
+
+	const isAllChecked = value.length === categories.length;
+
 	const handleFormSubmit = useCallback((): void => {
-		void handleSubmit(
-			(selectedCategoriesSubmissionData: {
-				categoryIds: SelectedCategoryIds;
-			}) => {
-				setSubmittedCategoryIds(selectedCategoriesSubmissionData.categoryIds);
-				// TODO: send selectedCategoriesSubmissionData to backend
-			},
-		)();
-	}, [handleSubmit]);
+		void handleSubmit(onSubmit)();
+	}, [handleSubmit, onSubmit]);
 
 	const handleAllValueChange = useCallback((): void => {
 		const nextCategoryIds = getAllCategoryIds(categories);
@@ -112,41 +88,36 @@ const CheckboxCategoriesForm: React.FC<Properties> = ({
 		[setValue, value],
 	);
 
-	return (
-		<LoaderWrapper isLoading={isFormLoading}>
-			{areCategoriesExist && (
-				<View style={[globalStyles.gap8, globalStyles.mb24]}>
-					<Checkbox
-						isChecked={isAllChecked}
-						label={CHECK_ALL_CATEGORIES_NAME}
-						onValueChange={handleAllValueChange}
-					/>
-					{categories.map(({ categoryName, id }) => {
-						const isChecked = value.includes(id);
+	if (categories.length === NumericalValue.ZERO) {
+		return null;
+	}
 
-						return (
-							<Checkbox
-								isChecked={isChecked}
-								key={id}
-								label={categoryName}
-								onValueChange={handleCategoryValueChange({ id, isChecked })}
-							/>
-						);
-					})}
-					<View style={globalStyles.alignItemsCenter}>
-						<Text color={BaseColor.RED}>
-							{errors[CATEGORIES_FIELD_NAME]?.message}
-						</Text>
-					</View>
-					<Button label={submitButtonName} onPress={handleFormSubmit} />
-					<View style={globalStyles.alignItemsCenter}>
-						<Text color={BaseColor.BLACK}>
-							{submittedCategoryIds.join(", ")}
-						</Text>
-					</View>
-				</View>
-			)}
-		</LoaderWrapper>
+	return (
+		<View style={[globalStyles.gap8, globalStyles.mb24]}>
+			<Checkbox
+				isChecked={isAllChecked}
+				label={CHECK_ALL_CATEGORIES_NAME}
+				onValueChange={handleAllValueChange}
+			/>
+			{categories.map(({ categoryName, id }) => {
+				const isChecked = value.includes(id);
+
+				return (
+					<Checkbox
+						isChecked={isChecked}
+						key={id}
+						label={categoryName}
+						onValueChange={handleCategoryValueChange({ id, isChecked })}
+					/>
+				);
+			})}
+			<View style={globalStyles.alignItemsCenter}>
+				<Text color={BaseColor.RED}>
+					{errors[CATEGORIES_FIELD_NAME]?.message}
+				</Text>
+			</View>
+			<Button label={submitButtonName} onPress={handleFormSubmit} />
+		</View>
 	);
 };
 
--- apps/mobile/src/screens/chat/chat.tsx
+++ apps/mobile/src/screens/chat/chat.tsx
@@ -1,19 +1,73 @@
-import React from "react";
-
 import {
 	CheckboxCategoriesForm,
 	ScreenWrapper,
+	ScrollView,
+	Text,
 	View,
 } from "~/libs/components/components";
+import { BaseColor } from "~/libs/enums/enums";
+import {
+	useAppDispatch,
+	useAppSelector,
+	useCallback,
+	useEffect,
+	useState,
+} from "~/libs/hooks/hooks";
 import { globalStyles } from "~/libs/styles/styles";
+import { type CategoriesSelectedRequestDto } from "~/libs/types/types";
+import { actions as categoriesActions } from "~/slices/quiz/quiz";
+
+import { ChatBox } from "./libs/components/components";
+import { styles } from "./styles";
 
 const Chat: React.FC = () => {
+	const [submittedCategoryIds, setSubmittedCategoryIds] = useState<
+		null | number[]
+	>(null);
+	const dispatch = useAppDispatch();
+	const categories = useAppSelector((state) => state.quiz.categories);
+
+	useEffect(() => {
+		void dispatch(categoriesActions.getScores());
+	}, [dispatch]);
+
+	const handleRetakeQuizSubmit = useCallback(
+		(payload: CategoriesSelectedRequestDto): void => {
+			setSubmittedCategoryIds(payload.categoryIds);
+			// TODO: send selectedCategoriesSubmissionData to backend
+		},
+		[],
+	);
+
 	return (
-		<ScreenWrapper>
-			<View style={[globalStyles.flex1, globalStyles.gap12, globalStyles.p12]}>
-				<CheckboxCategoriesForm submitButtonName="Retake Quiz" />
-			</View>
-		</ScreenWrapper>
+		<View style={[globalStyles.flex1, styles.container]}>
+			<ScreenWrapper>
+				<ScrollView
+					style={[
+						globalStyles.flex1,
+						globalStyles.pl24,
+						globalStyles.pt24,
+						globalStyles.pr16,
+						globalStyles.pb12,
+					]}
+				>
+					<View
+						style={[globalStyles.flex1, globalStyles.gap12, globalStyles.p12]}
+					>
+						<ChatBox>
+							<CheckboxCategoriesForm
+								categories={categories}
+								onSubmit={handleRetakeQuizSubmit}
+								submitButtonName="Retake Quiz"
+							/>
+						</ChatBox>
+						<Text color={BaseColor.BLACK}>
+							{submittedCategoryIds?.join(", ")}
+						</Text>
+					</View>
+				</ScrollView>
+			</ScreenWrapper>
+		</View>
 	);
 };
 
