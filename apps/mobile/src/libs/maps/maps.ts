e364902589db2c0e438a1f913f201e70af0b9006
Olga <okatranis@gmail.com>
2024-09-11 16:33:50 +0300

    feat: update export types, maps bb-344

--- apps/mobile/src/libs/maps/maps.ts
+++ apps/mobile/src/libs/maps/maps.ts
@@ -1,3 +1,4 @@
+export { categoryToColors } from "./category-to-colors/category-to-colors.map";
 export { colorToGradientColors } from "./color-to-gradient-colors/color-to-gradient-colors.map";
 export { directionToGradient } from "./direction-to-gradient/direction-to-gradient.map";
 export { fontWeightToFamily } from "./font-weight-to-family/font-weight-to-family-map";
