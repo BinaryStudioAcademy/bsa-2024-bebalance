894fa86247b20109e83bff20cbf401a9de481df1
Tetiana Brezhynska <brezhynska9012@gmail.com>
2024-09-11 12:11:38 +0200

    feat(mobile): added constant for checkbox categories initial data bb-250

---
+++ apps/mobile/src/libs/constants/checkbox-categories-initial-data.constant.ts
@@ -0,0 +1,5 @@
+import { type QuizScoresGetAllItemResponseDto } from "~/libs/types/types";
+
+const CHECKBOX_CATEGORIES_INITIAL_DATA: QuizScoresGetAllItemResponseDto[] = [];
+
+export { CHECKBOX_CATEGORIES_INITIAL_DATA };
--- apps/mobile/src/libs/constants/constants.ts
+++ apps/mobile/src/libs/constants/constants.ts
@@ -1,2 +1,3 @@
+export { CHECKBOX_CATEGORIES_INITIAL_DATA } from "./checkbox-categories-initial-data.constant";
 export { GRADIENT_SECTORS_INITIAL_DATA } from "./gradient-sectors-initial-data.constant";
 export { PREVIOUS_INDEX_OFFSET, ZERO_INDEX } from "shared";
