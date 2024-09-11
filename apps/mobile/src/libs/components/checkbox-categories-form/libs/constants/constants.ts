6dbcda5db5ef1f1d75fb94947132d595baf8e76d
Tetiana Brezhynska <brezhynska9012@gmail.com>
2024-09-11 12:36:22 +0200

    feat(mobile): added constants for CheckboxCategoriesForm  bb-250

---
+++ apps/mobile/src/libs/components/checkbox-categories-form/libs/constants/constants.ts
@@ -0,0 +1,13 @@
+import { type SelectedCategoryIds } from "../types/selected-category-ids.type";
+
+const CHECK_ALL_CATEGORIES_NAME = "All";
+const CATEGORIES_FIELD_NAME = "categoryIds";
+const CATEGORIES_FORM_DEFAULT_VALUES: SelectedCategoryIds = [];
+const EMPTY_ARRAY_LENGTH = 0;
+
+export {
+	CATEGORIES_FIELD_NAME,
+	CATEGORIES_FORM_DEFAULT_VALUES,
+	CHECK_ALL_CATEGORIES_NAME,
+	EMPTY_ARRAY_LENGTH,
+};
