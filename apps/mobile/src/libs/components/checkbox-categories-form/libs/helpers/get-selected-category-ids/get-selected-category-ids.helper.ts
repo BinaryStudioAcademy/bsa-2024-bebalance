5d1d9b18f753bab5559c5e82243e093f7bf359ee
Tetiana Brezhynska <brezhynska9012@gmail.com>
2024-09-11 18:37:37 +0200

    refactor(mobile): used shared ZERO_INDEX in getSelectedCategoryIds helper bb-250

--- apps/mobile/src/libs/components/checkbox-categories-form/libs/helpers/get-selected-category-ids/get-selected-category-ids.helper.ts
+++ apps/mobile/src/libs/components/checkbox-categories-form/libs/helpers/get-selected-category-ids/get-selected-category-ids.helper.ts
@@ -1,15 +1,15 @@
+import { ZERO_INDEX } from "~/libs/constants/constants";
 import { type QuizScoresGetAllItemResponseDto } from "~/libs/types/types";
 
 import { type SelectedCategoryIds } from "../../types/types";
 
-const FIRST_INDEX = 0;
 const LOWEST_SCORE_CATEGORIES_QUANTITY = 3;
 
 const getSelectedCategoryIds = (
 	sortedCategories: QuizScoresGetAllItemResponseDto[],
 ): SelectedCategoryIds => {
 	return sortedCategories
-		.slice(FIRST_INDEX, LOWEST_SCORE_CATEGORIES_QUANTITY)
+		.slice(ZERO_INDEX, LOWEST_SCORE_CATEGORIES_QUANTITY)
 		.map(({ id }) => id);
 };
 
