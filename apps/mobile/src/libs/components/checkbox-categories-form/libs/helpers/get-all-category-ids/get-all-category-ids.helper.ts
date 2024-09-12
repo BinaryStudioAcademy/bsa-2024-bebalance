28077aff2cf3b83088b6fc3e5ce6e455711f5af4
Tetiana Brezhynska <brezhynska9012@gmail.com>
2024-09-12 03:23:44 +0200

    refactor(mobile): removed getAllCategoryIds helper bb-250

--- apps/mobile/src/libs/components/checkbox-categories-form/libs/helpers/get-all-category-ids/get-all-category-ids.helper.ts
+++
@@ -1,9 +0,0 @@
-import { type QuizScoresGetAllItemResponseDto } from "~/libs/types/types";
-
-const getAllCategoryIds = (
-	categories: QuizScoresGetAllItemResponseDto[],
-): number[] => {
-	return categories.map(({ id }) => id);
-};
-
-export { getAllCategoryIds };
