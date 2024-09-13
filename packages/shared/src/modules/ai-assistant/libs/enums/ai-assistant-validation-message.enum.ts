bd78d96feb6c5c0f0b05da14d126f545cf3454ea
Andrii Levin <andriilevin@gmail.com>
2024-09-13 14:15:46 +0100

    feat(shared): add AiAssistantValidationMessage  bb-203

---
+++ packages/shared/src/modules/ai-assistant/libs/enums/ai-assistant-validation-message.enum.ts
@@ -0,0 +1,10 @@
+const AiAssistantValidationMessage = {
+	CATEGORIES_REQUIRED: "categories  required",
+	CATEGORY_ID_REQUIRED: "categoryId  required",
+	CATEGORY_NAME_REQUIRED: "categoryName  required",
+	TEXT_REQUIRED: "text  required",
+	THREAD_ID_INVALID_FORMAT: "threadId - invalid format",
+	THREAD_ID_REQUIRED: "threadId  required",
+} as const;
+
+export { AiAssistantValidationMessage };
