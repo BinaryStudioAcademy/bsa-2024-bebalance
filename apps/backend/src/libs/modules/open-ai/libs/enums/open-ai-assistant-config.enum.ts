6082c75ecb4701a252517f7afc1960a9c85e7f58
Andrii Levin <andriilevin@gmail.com>
2024-09-13 14:35:50 +0100

    refactor(backend): add more settings to  OpenAiAssistantConfig bb-203

--- apps/backend/src/libs/modules/open-ai/libs/enums/open-ai-assistant-config.enum.ts
+++ apps/backend/src/libs/modules/open-ai/libs/enums/open-ai-assistant-config.enum.ts
@@ -1,10 +1,15 @@
-import { AnalyzeBalanceScores } from "../ai-assistant-tools/ai-assistant-tools.js";
-import { OpenAiPromptTemplates } from "./open-ai-promt-mesagges.enum.js";
+import {
+	AnalyzeBalanceScoresTool,
+	GenerateTaskByCategoryTool,
+} from "../ai-assistant-tools/ai-assistant-tools.js";
+import { OpenAiPromptTemplates } from "./open-ai-prompt-messages.enum.js";
 
 const OpenAiAssistantConfig = {
 	INSTRUCTION: OpenAiPromptTemplates.ASSISTANT_INSTRUCTION,
 	NAME: "Wheel of Balance Assistant",
-	TOOLS: [AnalyzeBalanceScores],
+	TEMPERATURE: 1,
+	TOOLS: [AnalyzeBalanceScoresTool, GenerateTaskByCategoryTool],
+	TOP_P: 1,
 } as const;
 
 export { OpenAiAssistantConfig };
