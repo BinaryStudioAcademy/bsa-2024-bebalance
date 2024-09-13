656d5cd3087289c1d97d7cc86c6e3f0102ca89a4
Andrii Levin <andriilevin@gmail.com>
2024-09-13 14:16:42 +0100

    feat(shared): add addMessageToThread validation schema  bb-203

---
+++ packages/shared/src/modules/ai-assistant/libs/validation-schemas/add-message-to-thread.validation-schema.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+import {
+	AiAssistantValidationMessage,
+	AiAssistantValidationRule,
+} from "../enums/enums.js";
+
+type AiAssistantMessageCreateDto = {
+	text: z.ZodString;
+	threadId: z.ZodString;
+};
+
+const addMessageToThread = z
+	.object<AiAssistantMessageCreateDto>({
+		text: z
+			.string()
+			.trim()
+			.min(AiAssistantValidationRule.NON_EMPTY_STRING_MIN_LENGTH, {
+				message: AiAssistantValidationMessage.TEXT_REQUIRED,
+			}),
+		threadId: z
+			.string()
+			.trim()
+			.min(AiAssistantValidationRule.NON_EMPTY_STRING_MIN_LENGTH, {
+				message: AiAssistantValidationMessage.THREAD_ID_REQUIRED,
+			})
+			.regex(AiAssistantValidationRule.THREAD_ID_VALID_CHARS, {
+				message: AiAssistantValidationMessage.THREAD_ID_INVALID_FORMAT,
+			}),
+	})
+	.required();
+
+export { addMessageToThread };
