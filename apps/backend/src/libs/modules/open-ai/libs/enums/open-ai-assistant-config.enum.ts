0ae35ee954fa2b0ed1fed26d0103c10aae811648
Andrii Levin <andriilevin@gmail.com>
2024-09-14 17:45:51 +0100

    feat(backend): add OpenAi function for change task bb-203

--- apps/backend/src/libs/modules/open-ai/libs/enums/open-ai-assistant-config.enum.ts
+++ apps/backend/src/libs/modules/open-ai/libs/enums/open-ai-assistant-config.enum.ts
@@ -1,5 +1,6 @@
 import {
 	AnalyzeBalanceScoresTool,
+	ChangeTaskTool,
 	GenerateTaskByCategoryTool,
 } from "../ai-assistant-tools/ai-assistant-tools.js";
 import { OpenAiPromptTemplates } from "./open-ai-prompt-messages.enum.js";
@@ -8,7 +9,7 @@ const OpenAiAssistantConfig = {
 	INSTRUCTION: OpenAiPromptTemplates.ASSISTANT_INSTRUCTION,
 	NAME: "Wheel of Balance Assistant",
 	TEMPERATURE: 1,
-	TOOLS: [AnalyzeBalanceScoresTool, GenerateTaskByCategoryTool],
+	TOOLS: [AnalyzeBalanceScoresTool, GenerateTaskByCategoryTool, ChangeTaskTool],
 	TOP_P: 1,
 } as const;
 
--- apps/backend/src/libs/modules/open-ai/libs/enums/open-ai-function-name.enum.ts
+++ apps/backend/src/libs/modules/open-ai/libs/enums/open-ai-function-name.enum.ts
@@ -1,5 +1,6 @@
 const OpenAiFunctionName = {
 	ANALYZE_BALANCE_SCORES: "analyze_balance_scores",
+	CHANGE_TASK: "change_task",
 	GENERATE_TASK_BY_CATEGORY: "generate_task_by_category",
 } as const;
 export { OpenAiFunctionName };
