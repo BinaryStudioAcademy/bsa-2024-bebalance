4a9430a1c2dc48b955b1441983d9b844d1830a72
Andrii Levin <andriilevin@gmail.com>
2024-09-14 16:47:40 +0100

    feat(shared): add TaskCreateDto  bb-203

--- packages/shared/src/modules/tasks/libs/types/types.ts
+++ packages/shared/src/modules/tasks/libs/types/types.ts
@@ -1,2 +1,3 @@
+export { type TaskCreateDto } from "./task-create-dto.type.js";
 export { type TaskDto } from "./task-dto.type.js";
 export { type TaskGetAllResponseDto } from "./task-get-all-response-dto.type.js";
