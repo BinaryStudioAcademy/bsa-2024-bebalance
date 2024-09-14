4860161f4e283d862f7060a8d7202eb857619c16
Mikoyzskie <rapidstrike13@gmail.com>
2024-09-14 14:52:56 +0800

    fix(frontend): move logic from root to protected route bb-242

--- apps/frontend/src/pages/root/root.tsx
+++ apps/frontend/src/pages/root/root.tsx
@@ -1,20 +1,6 @@
-import { AppRoute } from "~/libs/enums/enums.js";
-import { useAppSelector, useEffect, useNavigate } from "~/libs/hooks/hooks.js";
-
 import { UserWheel } from "./components/components.js";
 
 const Root: React.FC = () => {
-	const navigate = useNavigate();
-	const { hasAnsweredQuizQuestions } = useAppSelector(({ auth }) => ({
-		hasAnsweredQuizQuestions: auth.user?.hasAnsweredQuizQuestions,
-	}));
-
-	useEffect(() => {
-		if (!hasAnsweredQuizQuestions) {
-			navigate(AppRoute.QUIZ);
-		}
-	}, [hasAnsweredQuizQuestions, navigate]);
-
 	return <UserWheel />;
 };
 
