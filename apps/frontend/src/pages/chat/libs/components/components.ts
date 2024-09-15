134b26d2855f30ba71126b890c2b746a580aa5b1
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-09-15 20:43:20 +0300

    refactor(frontend): clean code bb-340

---
+++ apps/frontend/src/pages/chat/libs/components/components.ts
@@ -0,0 +1,2 @@
+export { ChatMessage } from "./chat-message/chat-message.js";
+export { ConfirmationButtons } from "./confirmation-buttons/confirmation-buttons.js";
---
+++ apps/frontend/src/pages/chat/libs/helpers/helpers.ts
@@ -0,0 +1,1 @@
+export { handleButtonAction } from "./handle-button-action.helper.js";
