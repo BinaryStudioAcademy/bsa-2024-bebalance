1c4df157009a6d01fa523d65a7069c412d59bdbe
Andrii Levin <andriilevin@gmail.com>
2024-09-15 13:50:21 +0100

    refactor(shared): rename AiAssistantApiPath path bb-203

--- packages/shared/src/modules/ai-assistant/libs/enums/ai-assistant-api-path.enum.ts
+++ packages/shared/src/modules/ai-assistant/libs/enums/ai-assistant-api-path.enum.ts
@@ -1,9 +1,9 @@
 const AiAssistantApiPath = {
 	ACCEPT_TASK: "/chat/accept-task",
 	ADD_MESSAGE: "/chat/add-message",
+	CHANGE_TASK: "/chat/change-task",
 	CONTINUE_CHAT: "/chat/continue",
 	DELETE_CHAT: "/chat/remove",
-	GENERATE_ALTERNATIVE_TASK: "/chat/generate-alternative-task",
 	INIT_NEW_CHAT: "/chat/initiate",
 	SUGGEST_TASKS: "/chat/suggest-tasks",
 } as const;
