d6afc9bd38f3e0ae5a6eb36c9be315bbf2cc3734
Jake Rosales <65630066+JD-Rosales@users.noreply.github.com>
2024-09-16 14:09:16 +0800

    feat(frontend): New design for toast messages bb-372 (#435)
    * refactor(frontend): updated error message bb-372
    
    * feat(frontend): added notification icon svg's bb-372
    
    * feat(frontend): redesign toast notification bb-372

--- apps/frontend/src/libs/components/icon/libs/maps/icon-name-to-svg.map.ts
+++ apps/frontend/src/libs/components/icon/libs/maps/icon-name-to-svg.map.ts
@@ -3,13 +3,17 @@ import ClockActive from "~/assets/img/clock-active.svg?react";
 import ClockInactive from "~/assets/img/clock-inactive.svg?react";
 import Close from "~/assets/img/close.svg?react";
 import CrossedEye from "~/assets/img/crossed-eye.svg?react";
+import Error from "~/assets/img/error.svg?react";
 import Eye from "~/assets/img/eye.svg?react";
+import Info from "~/assets/img/info.svg?react";
 import Menu from "~/assets/img/menu.svg?react";
 import RoundedCheck from "~/assets/img/rounded-check.svg?react";
 import SettingsActive from "~/assets/img/settings-active.svg?react";
 import SettingsInactive from "~/assets/img/settings-inactive.svg?react";
+import Success from "~/assets/img/success.svg?react";
 import TasksActive from "~/assets/img/tasks-active.svg?react";
 import TasksInactive from "~/assets/img/tasks-inactive.svg?react";
+import Warning from "~/assets/img/warning.svg?react";
 import WheelOfBalanceActive from "~/assets/img/wheel-of-balance-active.svg?react";
 import WheelOfBalanceInactive from "~/assets/img/wheel-of-balance-inactive.svg?react";
 import { type IconName } from "~/libs/types/types.js";
@@ -23,13 +27,17 @@ const iconNameToSvg: Record<
 	clockInactive: ClockInactive,
 	close: Close,
 	crossedEye: CrossedEye,
+	error: Error,
 	eye: Eye,
+	info: Info,
 	menu: Menu,
 	roundedCheck: RoundedCheck,
 	settingsActive: SettingsActive,
 	settingsInactive: SettingsInactive,
+	success: Success,
 	tasksActive: TasksActive,
 	tasksInactive: TasksInactive,
+	warning: Warning,
 	wheelActive: WheelOfBalanceActive,
 	wheelInactive: WheelOfBalanceInactive,
 };
--- apps/frontend/src/libs/components/notification/notification.tsx
+++ apps/frontend/src/libs/components/notification/notification.tsx
@@ -1,8 +1,23 @@
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import { useCallback } from "~/libs/hooks/hooks.js";
+
+import { NotificationIcon } from "./libs/components/components.js";
+import { type IconProperties } from "./libs/types/types.js";
+
 const Notification: React.FC = () => {
-	return <ToastContainer />;
+	const handleRenderIcon = useCallback((context: IconProperties) => {
+		const { type } = context;
+
+		if (type === "default") {
+			return null;
+		}
+
+		return <NotificationIcon notificationType={type} />;
+	}, []);
+
+	return <ToastContainer hideProgressBar icon={handleRenderIcon} />;
 };
 
 export { Notification };
