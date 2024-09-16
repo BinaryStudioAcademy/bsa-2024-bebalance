c3e6f9a3b1347a99259b5c3256e63fcf48303211
Olga <okatranis@gmail.com>
2024-09-16 12:12:34 +0300

    feat: move text preset, style, size to global maps bb-344

--- apps/mobile/src/libs/maps/maps.ts
+++ apps/mobile/src/libs/maps/maps.ts
@@ -1,4 +1,7 @@
 export { categoryToColors } from "./category-to-colors/category-to-colors.map";
 export { colorToGradientColors } from "./color-to-gradient-colors/color-to-gradient-colors.map";
 export { directionToGradient } from "./direction-to-gradient/direction-to-gradient.map";
+export { fontWeightToFamily } from "./font-weight-to-family/font-weight-to-family-map";
+export { presetToTextStyle } from "./preset-to-text-style/preset-to-text-style-map";
 export { sizeToStyles } from "./size-to-styles/size-to-styles";
+export { sizeToTextStyle } from "./size-to-text-style/size-to-text-style.map";
