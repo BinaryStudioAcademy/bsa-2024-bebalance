894d991073385faca180125e9b5882a1033b8192
Olga <okatranis@gmail.com>
2024-09-16 12:43:16 +0300

    feat: rename file size to style map bb-344

--- apps/mobile/src/libs/maps/maps.ts
+++ apps/mobile/src/libs/maps/maps.ts
@@ -3,5 +3,5 @@ export { colorToGradientColors } from "./color-to-gradient-colors/color-to-gradi
 export { directionToGradient } from "./direction-to-gradient/direction-to-gradient.map";
 export { fontWeightToFamily } from "./font-weight-to-family/font-weight-to-family-map";
 export { presetToTextStyle } from "./preset-to-text-style/preset-to-text-style-map";
-export { sizeToStyles } from "./size-to-styles/size-to-styles";
+export { sizeToStyles } from "./size-to-styles/size-to-styles.map";
 export { sizeToTextStyle } from "./size-to-text-style/size-to-text-style.map";
