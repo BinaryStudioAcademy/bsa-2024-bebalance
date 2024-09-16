218bad9197faae48f5c6c2ac015f892647d7f464
Olga <okatranis@gmail.com>
2024-09-16 10:35:17 +0300

    feat: update form for slider  bb-344

---
+++ apps/mobile/src/screens/edit-wheel-results/libs/components/scores-edit-form/scores-edit-form.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+import {
+	Button,
+	MultipleSliderInput,
+	Text,
+	View,
+} from "~/libs/components/components";
+import { useAppForm, useCallback } from "~/libs/hooks/hooks";
+import { globalStyles } from "~/libs/styles/styles";
+import {
+	type QuizScoresGetAllItemResponseDto,
+	type QuizScoresUpdateRequestDto,
+	updateScoresValidationSchema,
+} from "~/packages/quiz/quiz";
+import { SCORE_FORM_DEFAULT_VALUES } from "~/screens/edit-wheel-results/libs/constants/constants";
+
+import { styles } from "./styles";
+
+type Properties = {
+	data: QuizScoresGetAllItemResponseDto[];
+	onSubmit: (payload: QuizScoresUpdateRequestDto) => void;
+};
+
+const ScoresEditForm: React.FC<Properties> = ({ data, onSubmit }) => {
+	const { control, handleSubmit } = useAppForm<QuizScoresUpdateRequestDto>({
+		defaultValues: SCORE_FORM_DEFAULT_VALUES,
+		validationSchema: updateScoresValidationSchema,
+	});
+
+	const handleFormSubmit = useCallback((): void => {
+		void handleSubmit((formData) => {
+			onSubmit(formData);
+		})();
+	}, [handleSubmit, onSubmit]);
+
+	const sliderValues = data.map(({ categoryId, categoryName, score }) => {
+		return {
+			id: categoryId,
+			label: categoryName,
+			value: score,
+		};
+	});
+
+	return (
+		<View
+			style={[
+				globalStyles.flex1,
+				globalStyles.pv16,
+				globalStyles.mh12,
+				styles.form,
+			]}
+		>
+			<View style={globalStyles.alignItemsCenter}>
+				<Text weight="bold">Do you feel any changes in anything?</Text>
+				<Text weight="bold">Estimate the fields from 1 to 10</Text>
+			</View>
+			<MultipleSliderInput control={control} data={sliderValues} name="items" />
+			<View style={[globalStyles.mh32, globalStyles.pt24]}>
+				<Button label="SAVE CHANGES" onPress={handleFormSubmit} />
+			</View>
+		</View>
+	);
+};
+
+export { ScoresEditForm };
