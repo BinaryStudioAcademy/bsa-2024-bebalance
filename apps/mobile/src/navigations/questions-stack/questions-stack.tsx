b21f8976fce3de1ea73a96f7d539e35cbde331d1
KotenkoNata <kotenkonata8@gmail.com>
2024-09-17 20:55:34 +0200

    feat(mobile): hasAnsweredOnboardingQuestions, hasNotificationDays bb-360

--- apps/mobile/src/navigations/questions-stack/questions-stack.tsx
+++ apps/mobile/src/navigations/questions-stack/questions-stack.tsx
@@ -1,13 +1,14 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 
-import { QuestionsStackName } from "~/libs/enums/enums";
+import { NumericalValue, QuestionsStackName } from "~/libs/enums/enums";
+import { useAppSelector } from "~/libs/hooks/hooks";
 import { type QuestionsStackNavigationParameterList } from "~/libs/types/types";
 import { BottomTabsNavigator } from "~/navigations/bottom-tabs-navigator/bottom-tabs-navigator";
 import { NotificationQuestions } from "~/screens/notification-questions/notification-questions";
 import { Onboarding } from "~/screens/onboarding/onboarding";
-import { Quiz } from "~/screens/quiz/quiz";
 import { QuizEntry } from "~/screens/quiz-entry/quiz-entry";
+import { Quiz } from "~/screens/quiz/quiz";
 import { Welcome } from "~/screens/welcome/welcome";
 import { WheelLoading } from "~/screens/wheel-loading/wheel-loading";
 
@@ -15,25 +16,43 @@ const NativeStack =
 	createNativeStackNavigator<QuestionsStackNavigationParameterList>();
 
 const QuestionsStack: React.FC = () => {
+	const hasAnsweredOnboardingQuestions = useAppSelector(
+		({ auth }) => auth.user?.hasAnsweredOnboardingQuestions,
+	);
+
+	const hasNotificationDays = useAppSelector(
+		({ auth }) =>
+			(auth.user?.userTaskDays?.length ?? NumericalValue.ZERO) >
+			NumericalValue.ZERO,
+	);
+
 	return (
 		<NativeStack.Navigator screenOptions={{ headerShown: false }}>
-			<NativeStack.Screen
-				component={Onboarding}
-				name={QuestionsStackName.ONBOARDING}
-			/>
-			<NativeStack.Screen
-				component={Welcome}
-				name={QuestionsStackName.WELCOME}
-			/>
+			{!hasAnsweredOnboardingQuestions && (
+				<>
+					<NativeStack.Screen
+						component={Onboarding}
+						name={QuestionsStackName.ONBOARDING}
+					/>
+					<NativeStack.Screen
+						component={Welcome}
+						name={QuestionsStackName.WELCOME}
+					/>
+				</>
+			)}
 			<NativeStack.Screen
 				component={QuizEntry}
 				name={QuestionsStackName.QUIZ_ENTRY}
 			/>
 			<NativeStack.Screen component={Quiz} name={QuestionsStackName.QUIZ} />
-			<NativeStack.Screen
-				component={NotificationQuestions}
-				name={QuestionsStackName.NOTIFICATION_QUESTIONS}
-			/>
+			{!hasNotificationDays && (
+				<>
+					<NativeStack.Screen
+						component={NotificationQuestions}
+						name={QuestionsStackName.NOTIFICATION_QUESTIONS}
+					/>
+				</>
+			)}
 			<NativeStack.Screen
 				component={WheelLoading}
 				name={QuestionsStackName.WHEEL_LOADING}
