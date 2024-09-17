f5ffaa7eec45dbc06f15888460aea76911b45ad3
Tetiana Brezhynska <brezhynska9012@gmail.com>
2024-09-17 02:11:07 +0200

    feat(mobile): added app reducer to store bb-358

--- apps/mobile/src/libs/packages/store/store.package.ts
+++ apps/mobile/src/libs/packages/store/store.package.ts
@@ -10,6 +10,7 @@ import { authApi } from "~/packages/auth/auth";
 import { onboardingApi } from "~/packages/onboarding/onboarding";
 import { quizApi } from "~/packages/quiz/quiz";
 import { userApi } from "~/packages/users/users";
+import { reducer as appReducer } from "~/slices/app/app";
 import { reducer as authReducer } from "~/slices/auth/auth";
 import { reducer as onboardingReducer } from "~/slices/onboarding/onboarding";
 import { reducer as quizReducer } from "~/slices/quiz/quiz";
@@ -19,9 +20,11 @@ import { type Config } from "../config/config";
 import { handleErrorMiddleware } from "./middleware/handle-error.middleware";
 
 type RootReducer = {
+	app: ReturnType<typeof appReducer>;
 	auth: ReturnType<typeof authReducer>;
 	onboarding: ReturnType<typeof onboardingReducer>;
 	quiz: ReturnType<typeof quizReducer>;
+	users: ReturnType<typeof usersReducer>;
 };
 
 type ExtraArguments = {
@@ -51,6 +54,7 @@ class Store {
 				}).prepend([handleErrorMiddleware]);
 			},
 			reducer: {
+				app: appReducer,
 				auth: authReducer,
 				onboarding: onboardingReducer,
 				quiz: quizReducer,
