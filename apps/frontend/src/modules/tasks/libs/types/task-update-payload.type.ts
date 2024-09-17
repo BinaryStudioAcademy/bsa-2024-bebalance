e87041df893b63875514cae044bb496e3979cb10
marharita08 <rozgon.margarita08@gmail.com>
2024-09-17 14:02:44 +0300

    feat(frontend): add actions to update task and to get past tasks to tasks slice bb-362

---
+++ apps/frontend/src/modules/tasks/libs/types/task-update-payload.type.ts
@@ -0,0 +1,8 @@
+import { type TaskUpdateRequestDto } from "shared";
+
+type TaskUpdatePayload = {
+	data: TaskUpdateRequestDto;
+	id: number;
+};
+
+export { type TaskUpdatePayload };
--- apps/frontend/src/modules/tasks/slices/actions.ts
+++ apps/frontend/src/modules/tasks/slices/actions.ts
@@ -3,6 +3,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { type AsyncThunkConfig } from "~/libs/types/types.js";
 import { type TaskDto } from "~/modules/tasks/tasks.js";
 
+import { type TaskUpdatePayload } from "../libs/types/types.js";
 import { name as sliceName } from "./tasks.slice.js";
 
 const getCurrentTasks = createAsyncThunk<
@@ -15,4 +16,23 @@ const getCurrentTasks = createAsyncThunk<
 	return await tasksApi.getCurrentTasks();
 });
 
-export { getCurrentTasks };
+const getPastTasks = createAsyncThunk<TaskDto[], undefined, AsyncThunkConfig>(
+	`${sliceName}/get-past-tasks`,
+	async (_, { extra }) => {
+		const { tasksApi } = extra;
+
+		return await tasksApi.getPastTasks();
+	},
+);
+
+const update = createAsyncThunk<TaskDto, TaskUpdatePayload, AsyncThunkConfig>(
+	`${sliceName}/get-current-tasks`,
+	async (payload, { extra }) => {
+		const { tasksApi } = extra;
+		const { data, id } = payload;
+
+		return await tasksApi.update(id, data);
+	},
+);
+
+export { getCurrentTasks, getPastTasks, update };
