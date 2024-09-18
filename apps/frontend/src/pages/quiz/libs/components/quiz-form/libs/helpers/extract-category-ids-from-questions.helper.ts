d6da80047cb1eef45c98ff7b838e2bc94e82cc3e
ihorLysak <lysak.ihor4221@gmail.com>
2024-09-18 15:14:45 +0300

    refactor: added first item index constant bb-357

--- apps/frontend/src/pages/quiz/libs/components/quiz-form/libs/helpers/extract-category-ids-from-questions.helper.ts
+++ apps/frontend/src/pages/quiz/libs/components/quiz-form/libs/helpers/extract-category-ids-from-questions.helper.ts
@@ -1,4 +1,4 @@
-import { ZERO_INDEX } from "~/libs/constants/constants.js";
+import { FIRST_ITEM_INDEX } from "~/libs/constants/constants.js";
 import { type QuizQuestionDto } from "~/modules/quiz/quiz.js";
 
 function extractCategoryIdsFromQuestions(
@@ -8,7 +8,7 @@ function extractCategoryIdsFromQuestions(
 
 	for (const questionsInsideCategory of questions) {
 		categoryIds.add(
-			(questionsInsideCategory[ZERO_INDEX] as QuizQuestionDto).categoryId,
+			(questionsInsideCategory[FIRST_ITEM_INDEX] as QuizQuestionDto).categoryId,
 		);
 	}
 
