3c0ab41cae530739294fed76ff13d20d8f9fb2cb
Andrii Levin <andriilevin@gmail.com>
2024-09-19 20:48:11 +0100

    feat(shared): add validation message to  AIAssistantValidationMessage    bb-203

--- packages/shared/src/modules/ai-assistant/libs/enums/enums.ts
+++ packages/shared/src/modules/ai-assistant/libs/enums/enums.ts
@@ -1,3 +1,3 @@
-export { AiAssistantApiPath } from "./ai-assistant-api-path.enum.js";
-export { AiAssistantValidationMessage } from "./ai-assistant-validation-message.enum.js";
+export { AIAssistantApiPath } from "./ai-assistant-api-path.enum.js";
+export { AIAssistantValidationMessage } from "./ai-assistant-validation-message.enum.js";
 export { AiAssistantValidationRule } from "./ai-assistant-validatuon-rule.enum.js";
