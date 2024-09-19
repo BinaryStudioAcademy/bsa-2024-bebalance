9950e2eedb541364552b6a45aafe99aef2fe5e99
marharita08 <rozgon.margarita08@gmail.com>
2024-09-19 19:25:12 +0300

    fix(frontend): update tasks slice bb-362

--- apps/frontend/src/modules/tasks/slices/tasks.slice.ts
+++ apps/frontend/src/modules/tasks/slices/tasks.slice.ts
@@ -4,7 +4,6 @@ import { DataStatus } from "~/libs/enums/enums.js";
 import { type ValueOf } from "~/libs/types/types.js";
 import { type TaskDto } from "~/modules/tasks/tasks.js";
 
-import { NumericalValue } from "../libs/enums/enums.js";
 import { getCurrentTasks, getPastTasks, update } from "./actions.js";
 
 type State = {
@@ -46,13 +45,9 @@ const { actions, name, reducer } = createSlice({
 		});
 		builder.addCase(update.fulfilled, (state, action) => {
 			state.dataStatus = DataStatus.FULFILLED;
-			const index = state.tasks.findIndex(
-				(task) => task.id === action.payload.id,
-			);
-			state.tasks = [
-				...state.tasks.slice(NumericalValue.ZERO, index),
-				...state.tasks.slice(index + NumericalValue.ONE),
-			];
+			state.tasks = state.tasks.filter((task) => {
+				return task.id !== action.payload.id;
+			});
 		});
 		builder.addCase(update.rejected, (state) => {
 			state.dataStatus = DataStatus.REJECTED;
