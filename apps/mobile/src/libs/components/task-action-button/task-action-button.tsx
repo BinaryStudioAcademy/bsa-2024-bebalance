e4dcfdf0d384f545484bf7671629cba20806c7eb
Olga <okatranis@gmail.com>
2024-09-19 12:26:05 +0300

    feat: add task-action-button bb-321

---
+++ apps/mobile/src/libs/components/task-action-button/task-action-button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Text, View } from "~/libs/components/components";
+
+const TaskActionButton = (): JSX.Element => {
+	return (
+		<View>
+			<Text>BUTTON</Text>
+		</View>
+	);
+};
+
+export { TaskActionButton };
