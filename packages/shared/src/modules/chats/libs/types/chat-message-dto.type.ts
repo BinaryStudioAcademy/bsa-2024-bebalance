29d42a6916dba78e75f4df27411dabca12c82f27
Andrii Levin <andriilevin@gmail.com>
2024-09-19 20:36:04 +0100

    feat(shared): add ChatMessageDto type  bb-203

---
+++ packages/shared/src/modules/chats/libs/types/chat-message-dto.type.ts
@@ -0,0 +1,26 @@
+import { type ValueOf } from "../../../../libs/types/types.js";
+import {
+	type ChatMessageAuthor,
+	type ChatMessageType,
+} from "../enums/enums.js";
+import { type BalanceWheelMessage } from "./balance-wheel-message.type.js";
+import { type QuestionMessage } from "./question-message.type.js";
+import { type TaskMessage } from "./task-message.type.js";
+import { type TextMessage } from "./text.message.type.js";
+
+type ChatMessagePayload =
+	| BalanceWheelMessage
+	| QuestionMessage
+	| TaskMessage
+	| TextMessage;
+
+type ChatMessageDto = {
+	author: ValueOf<typeof ChatMessageAuthor>;
+	createdAt: string;
+	id: number;
+	isRead: boolean;
+	payload: ChatMessagePayload;
+	type: ValueOf<typeof ChatMessageType>;
+};
+
+export { type ChatMessageDto };
