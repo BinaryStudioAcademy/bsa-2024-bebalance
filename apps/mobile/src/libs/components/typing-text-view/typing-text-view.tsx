0388a999a71c23fbecf1e9c42e66f2ddfe624f1c
Tetiana Brezhynska <brezhynska9012@gmail.com>
2024-09-19 20:13:38 +0200

    refactor(mobile): added TypingTextAnimation enum bb-378

--- apps/mobile/src/libs/components/typing-text-view/typing-text-view.tsx
+++ apps/mobile/src/libs/components/typing-text-view/typing-text-view.tsx
@@ -4,12 +4,8 @@ import { Text } from "~/libs/components/components";
 import { BaseColor } from "~/libs/enums/enums";
 import { type ComponentProps, type ValueOf } from "~/libs/types/types";
 
-import {
-	DEFAULT_START_TYPING_DELAY,
-	FIRST_LETTER_INDEX,
-	LETTERS_PRINTED_PER_STEP,
-	NEXT_LETTERS_PRINTED_DELAY,
-} from "./libs/constants/constants";
+import { FIRST_LETTER_INDEX } from "./libs/constants/constants";
+import { TypingTextAnimation } from "./libs/enums/enums";
 import { useTypingText } from "./libs/hooks/hooks";
 
 type Properties = {
@@ -23,10 +19,10 @@ type Properties = {
 
 const TypingTextView: React.FC<Properties> = ({
 	children,
-	lettersPrintedPerStep = LETTERS_PRINTED_PER_STEP,
+	lettersPrintedPerStep = TypingTextAnimation.LETTERS_PRINTED_PER_STEP,
 	maskingColor = BaseColor.BG_WHITE,
-	nextLettersPrintedDelay = NEXT_LETTERS_PRINTED_DELAY,
-	startTypingDelay = DEFAULT_START_TYPING_DELAY,
+	nextLettersPrintedDelay = TypingTextAnimation.NEXT_LETTERS_PRINTED_DELAY,
+	startTypingDelay = TypingTextAnimation.DEFAULT_START_TYPING_DELAY,
 	textColor = BaseColor.BLACK,
 	...textProperties
 }: Properties) => {
