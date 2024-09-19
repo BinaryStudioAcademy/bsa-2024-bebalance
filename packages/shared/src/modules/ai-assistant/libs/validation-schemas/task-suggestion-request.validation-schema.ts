8dfbf2c0e5a0ea7b9d4b943aec68bedab9db520b
Andrii Levin <andriilevin@gmail.com>
2024-09-19 21:00:13 +0100

    refactor(shared): change  taskSuggestionRequest validation to have payload and lastMessageId bb-203

--- packages/shared/src/modules/ai-assistant/libs/validation-schemas/task-suggestion-request.validation-schema.ts
+++ packages/shared/src/modules/ai-assistant/libs/validation-schemas/task-suggestion-request.validation-schema.ts
@@ -1,37 +1,40 @@
 import { z } from "zod";
 
 import {
-	AiAssistantValidationMessage,
+	AIAssistantValidationMessage,
 	AiAssistantValidationRule,
 } from "../enums/enums.js";
 
-const taskSuggestionRequest = z.object({
-	categories: z
-		.array(
-			z.object({
-				categoryId: z
-					.number()
-					.min(AiAssistantValidationRule.NON_EMPTY_ITEM_MIN_LENGTH, {
-						message: AiAssistantValidationMessage.CATEGORY_ID_REQUIRED,
-					}),
-				categoryName: z
-					.string()
-					.min(AiAssistantValidationRule.NON_EMPTY_ITEM_MIN_LENGTH, {
-						message: AiAssistantValidationMessage.CATEGORY_NAME_REQUIRED,
-					}),
-			}),
-		)
-		.nonempty({
-			message: AiAssistantValidationMessage.CATEGORIES_REQUIRED,
+const selectedCategory = z.object({
+	categoryId: z
+		.number()
+		.min(AiAssistantValidationRule.NON_EMPTY_ITEM_MIN_LENGTH, {
+			message: AIAssistantValidationMessage.CATEGORY_ID_REQUIRED,
 		}),
+	categoryName: z
+		.string()
+		.min(AiAssistantValidationRule.NON_EMPTY_ITEM_MIN_LENGTH, {
+			message: AIAssistantValidationMessage.CATEGORY_NAME_REQUIRED,
+		}),
+});
+
+const taskSuggestionRequest = z.object({
+	lastMessageId: z
+		.number()
+		.min(AiAssistantValidationRule.NON_EMPTY_ITEM_MIN_LENGTH, {
+			message: AIAssistantValidationMessage.LAST_MESSAGE_ID_REQUIRED,
+		}),
+	payload: z
+		.array(selectedCategory)
+		.nonempty({ message: AIAssistantValidationMessage.CATEGORIES_REQUIRED }),
 	threadId: z
 		.string()
 		.trim()
 		.min(AiAssistantValidationRule.NON_EMPTY_ITEM_MIN_LENGTH, {
-			message: AiAssistantValidationMessage.THREAD_ID_REQUIRED,
+			message: AIAssistantValidationMessage.THREAD_ID_REQUIRED,
 		})
 		.regex(AiAssistantValidationRule.THREAD_ID_VALID_CHARS, {
-			message: AiAssistantValidationMessage.THREAD_ID_INVALID_FORMAT,
+			message: AIAssistantValidationMessage.THREAD_ID_INVALID_FORMAT,
 		}),
 });
 
