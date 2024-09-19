cf1cd9bea82270c672608cce73baa3fb5d5ac9df
Andrii Levin <andriilevin@gmail.com>
2024-09-19 20:46:12 +0100

    feat(shared): add explain task path to AIAssistantApiPath    bb-203

--- packages/shared/src/modules/ai-assistant/libs/enums/ai-assistant-api-path.enum.ts
+++ packages/shared/src/modules/ai-assistant/libs/enums/ai-assistant-api-path.enum.ts
@@ -1,10 +1,11 @@
-const AiAssistantApiPath = {
+const AIAssistantApiPath = {
 	CHAT_ACCEPT_TASK: "/chat/accept-task",
 	CHAT_ADD_MESSAGE: "/chat/add-message",
 	CHAT_CHANGE_TASK: "/chat/change-task",
 	CHAT_CONTINUE: "/chat/continue",
+	CHAT_EXPLAIN_TASK: "/chat/explain-task",
 	CHAT_INITIATE: "/chat/initiate",
 	CHAT_SUGGEST_TASKS: "/chat/suggest-tasks",
 } as const;
 
-export { AiAssistantApiPath };
+export { AIAssistantApiPath };
