b7549a9f824c0396d6fdd1d4936c0a67ea67c910
Olga <okatranis@gmail.com>
2024-09-20 12:18:30 +0300

    feat: add get-pas-task and update-tsk to actions bb-321

--- apps/mobile/src/slices/task/actions.ts
+++ apps/mobile/src/slices/task/actions.ts
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 import { type AsyncThunkConfig } from "~/libs/types/types";
-import { type TaskDto } from "~/packages/tasks/tasks";
+import { type TaskDto, type TaskUpdatePayload } from "~/packages/tasks/tasks";
 
 import { name as sliceName } from "./task.slice";
 
@@ -15,4 +15,24 @@ const getCurrentTasks = createAsyncThunk<
 	return await tasksApi.getCurrentTasks();
 });
 
-export { getCurrentTasks };
+const getPastTasks = createAsyncThunk<TaskDto[], undefined, AsyncThunkConfig>(
+	`${sliceName}/get-past-tasks`,
+	async (_, { extra }) => {
+		const { tasksApi } = extra;
+
+		return await tasksApi.getPastTasks();
+	},
+);
+
+const updateTask = createAsyncThunk<
+	TaskDto,
+	TaskUpdatePayload,
+	AsyncThunkConfig
+>(`${sliceName}/update-task`, async (payload, { extra }) => {
+	const { tasksApi } = extra;
+	const { id, ...data } = payload;
+
+	return await tasksApi.updateTask(id, data);
+});
+
+export { getCurrentTasks, getPastTasks, updateTask };
