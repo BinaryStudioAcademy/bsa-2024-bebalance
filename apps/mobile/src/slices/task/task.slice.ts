84ee8b0ddd56e04ca60d4aa7a27165f1d75a403c
Olha Bainurenova <77403180+Olha-Katran@users.noreply.github.com>
2024-09-23 17:39:34 +0300

    feat(mobile):  task card bb-321 (#511)
    * feat: add task actions bb-321
    
    * feat: create taskApi bb-321
    
    * feat: export tasks type and enum bb-321
    
    * feat: create tasks-slice bb-321
    
    * feat: update store bb-321
    
    * feat: export allActions as actions bb-321
    
    * feat: add task-action-button bb-321
    
    * feat: deadline component bb-321
    
    * feat: create task-card component bb-321
    
    * feat: add usage task-card component bb-321
    
    * feat: add clock-active-svg, add usage icon, refactor style for card-header  bb-321
    
    * feat: add get-pas-task and update-tsk to actions bb-321
    
    * feat: update tag component (remove color props) bb-321
    
    * feat: create tasksMode enum bb-321
    
    * feat: create task-update-payload.type bb-321
    
    * feat: update task-action-button bb-321
    
    * feat: update task-api - updateTask, getPastTasks bb-321
    
    * feat: update slice (updateTask, getPastTasks) bb-321
    
    * feat: update styles bb-321
    
    * feat: task screen - add switch between past nd active, add query for past tasks bb-321
    
    * feat: add main components to task-card bb-321
    
    * feat: add new baseColor value and usage bb-321
    
    * feat: create enum for task tab bb-321
    
    * feat: change style for deadline component bb-321
    
    * feat: change style for deadline component bb-321
    
    * feat: rename deadline to deadline-countdown bb-321
    
    * feat: move export from enums file bb-321
    
    * feat: rename function - handleCalculateDaysUntilDeadline bb-321
    
    * feat: create separate task-status-label bb-321
    
    * feat: add useMemo for statusProperties bb-321
    
    * feat: add height for description bb-321
    
    * feat: add type for status-prop bb-321
    
    * refactor: move enums and constants to separate files  bb-321
    
    * refactor: move get-status-prop to separate helper bb-321
    
    * feat: add users to root-reducer bb-321
    
    * feat: add partial to task-update-request-dto bb-321
    
    * feat: remove unnecessary properties bb-321
    
    * feat: move get-formatted-countdown-units to separate helper bb-321
    
    * feat: add custom use-countdown hook bb-321
    
    * refactor: remove type bb-321
    
    * refactor: update deadline-countdown to destructure countdown values bb-321
    
    * refactor: delete unnecessary view with style bb-321
    
    * refactor: move style to page-switcher directly bb-321
    
    * refactor: split calculation and expiration check into separate functions bb-321

---
+++ apps/mobile/src/slices/task/task.slice.ts
@@ -0,0 +1,60 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+import { DataStatus } from "~/libs/enums/enums";
+import { type ValueOf } from "~/libs/types/types";
+import { type TaskDto } from "~/packages/tasks/tasks";
+
+import { getCurrentTasks, getPastTasks, updateTask } from "./actions";
+
+type State = {
+	dataStatus: ValueOf<typeof DataStatus>;
+	tasks: TaskDto[];
+};
+
+const initialState: State = {
+	dataStatus: DataStatus.IDLE,
+	tasks: [],
+};
+
+const { actions, name, reducer } = createSlice({
+	extraReducers(builder) {
+		builder.addCase(getCurrentTasks.pending, (state) => {
+			state.dataStatus = DataStatus.PENDING;
+		});
+		builder.addCase(getCurrentTasks.fulfilled, (state, action) => {
+			state.dataStatus = DataStatus.FULFILLED;
+			state.tasks = action.payload;
+		});
+		builder.addCase(getCurrentTasks.rejected, (state) => {
+			state.dataStatus = DataStatus.REJECTED;
+		});
+		builder.addCase(getPastTasks.pending, (state) => {
+			state.dataStatus = DataStatus.PENDING;
+		});
+		builder.addCase(getPastTasks.fulfilled, (state, action) => {
+			state.dataStatus = DataStatus.FULFILLED;
+			state.tasks = action.payload;
+		});
+		builder.addCase(getPastTasks.rejected, (state) => {
+			state.dataStatus = DataStatus.REJECTED;
+		});
+
+		builder.addCase(updateTask.pending, (state) => {
+			state.dataStatus = DataStatus.PENDING;
+		});
+		builder.addCase(updateTask.fulfilled, (state, action) => {
+			state.dataStatus = DataStatus.FULFILLED;
+			state.tasks = state.tasks.filter((task) => {
+				return task.id !== action.payload.id;
+			});
+		});
+		builder.addCase(updateTask.rejected, (state) => {
+			state.dataStatus = DataStatus.REJECTED;
+		});
+	},
+	initialState,
+	name: "tasks",
+	reducers: {},
+});
+
+export { actions, name, reducer };
