e5c4a6326a4c503ea5e226d340cf809a7c943ae1
Andrii Levin <andriilevin@gmail.com>
2024-09-24 22:17:59 +0100

    refactor(backend): fix import and name in AI helpers  bb-340

--- apps/backend/src/modules/ai-assistant/libs/helpers/helpers.ts
+++ apps/backend/src/modules/ai-assistant/libs/helpers/helpers.ts
@@ -1,4 +1,12 @@
 export {
+	generateChangeTasksSuggestionsResponse,
+	runChangeTasksByCategoryOptions,
+} from "./change-tasks/run-change-task-by-category-options.helper.js";
+export {
+	generateExplainTasksSuggestionsResponse,
+	runExplainTaskOptions,
+} from "./explain-tasks/run-explain-tasks-option.helper.js";
+export {
 	generateQuestionsAnswersPrompt,
 	generateUserScoresPrompt,
 } from "./initial-chat/initial-chat.js";
@@ -6,11 +14,3 @@ export {
 	generateTasksSuggestionsResponse,
 	runSuggestTaskByCategoryOptions,
 } from "./suggest-task-by-category/run-suggest-task-by-category-option.helper.js";
-export {
-	generateChangeTasksSuggestionsResponse,
-	runChangeTasksByCategoryOptions,
-} from "~/modules/ai-assistant/libs/helpers/change-tasks/run-change-task-by-category-options.helper.js";
-export {
-	generateExplainTasksSuggestionsResponse,
-	runExplainTaskOptions,
-} from "~/modules/ai-assistant/libs/helpers/explain-tasks/run-explain-tasks-option.helper.js";
