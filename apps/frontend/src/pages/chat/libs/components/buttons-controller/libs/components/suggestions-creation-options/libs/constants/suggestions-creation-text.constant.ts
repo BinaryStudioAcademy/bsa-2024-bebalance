c37f7e909dd1fc6f965a9ff435ce4f6df891a5fa
ihorLysak <lysak.ihor4221@gmail.com>
2024-09-24 20:42:51 +0300

    refactor: changed suggestions creation text bb-340

--- apps/frontend/src/pages/chat/libs/components/buttons-controller/libs/components/suggestions-creation-options/libs/constants/suggestions-creation-text.constant.ts
+++ apps/frontend/src/pages/chat/libs/components/buttons-controller/libs/components/suggestions-creation-options/libs/constants/suggestions-creation-text.constant.ts
@@ -1,4 +1,4 @@
 const SUGGESTIONS_CREATION_TEXT =
-	"Do you want to work on 3 fields, with the lowest score, or you want to choose the fields to work on by yourself?";
+	"Do you want to work on 3 fields, with the lowest score, or you want to choose the fields to improve on your own?";
 
 export { SUGGESTIONS_CREATION_TEXT };
