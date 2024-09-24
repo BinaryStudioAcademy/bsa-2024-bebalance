ccda4271dec5c717a665d57d031153bcb08352a8
Andrii Levin <andriilevin@gmail.com>
2024-09-24 15:03:53 +0100

    refactor(shared): change  AIAssistantApiPath bb-340

--- packages/shared/src/modules/ai-assistant/libs/enums/ai-assistant-api-path.enum.ts
+++ packages/shared/src/modules/ai-assistant/libs/enums/ai-assistant-api-path.enum.ts
@@ -2,7 +2,7 @@ const AIAssistantApiPath = {
 	CHAT_ACCEPT_TASK: "/chat/accept-task",
 	CHAT_ACCEPT_TASKS: "/chat/accept-tasks",
 	CHAT_ADD_MESSAGE: "/chat/add-message",
-	CHAT_CHANGE_TASK: "/chat/change-tasks",
+	CHAT_CHANGE_TASKS: "/chat/change-tasks",
 	CHAT_CONTINUE: "/chat/continue",
 	CHAT_EXPLAIN_TASKS: "/chat/explain-tasks",
 	CHAT_INITIALIZE: "/chat/initialize",
