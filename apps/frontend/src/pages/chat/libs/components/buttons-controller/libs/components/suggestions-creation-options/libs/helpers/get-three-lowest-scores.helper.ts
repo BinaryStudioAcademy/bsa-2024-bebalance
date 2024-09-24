467e598f168bf96206a2dd46f1902e007f1ddced
ihorLysak <lysak.ihor4221@gmail.com>
2024-09-24 12:55:36 +0300

    refactor: rewritten getThreeLowestScores into an arrow function bb-340

--- apps/frontend/src/pages/chat/libs/components/buttons-controller/libs/components/suggestions-creation-options/libs/helpers/get-three-lowest-scores.helper.ts
+++ apps/frontend/src/pages/chat/libs/components/buttons-controller/libs/components/suggestions-creation-options/libs/helpers/get-three-lowest-scores.helper.ts
@@ -1,12 +1,12 @@
 import { NumericalValue } from "~/libs/enums/enums.js";
 import { type QuizScoresGetAllItemResponseDto } from "~/modules/quiz/quiz.js";
 
-function getThreeLowestScores(
+const getThreeLowestScores = (
 	categories: QuizScoresGetAllItemResponseDto[],
-): QuizScoresGetAllItemResponseDto[] {
+): QuizScoresGetAllItemResponseDto[] => {
 	const categoriesSorted = categories.toSorted((a, b) => a.score - b.score);
 
 	return categoriesSorted.slice(NumericalValue.ZERO, NumericalValue.THREE);
-}
+};
 
 export { getThreeLowestScores };
