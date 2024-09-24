e10f857a424fe2a655ac24f8e97a413c5b48b18e
luiqor <vohnenna@gmail.com>
2024-09-24 16:29:18 +0300

    refactor(mobile/shared): update getFormattedDate argument bb-494

--- packages/shared/src/libs/helpers/get-formatted-date/get-formatted-date.helper.ts
+++ packages/shared/src/libs/helpers/get-formatted-date/get-formatted-date.helper.ts
@@ -2,9 +2,7 @@ import { format } from "date-fns";
 
 const WHEEL_DATE_FORMAT = "d MMM yyyy, EEEE";
 
-const getFormattedDate = (inputDate?: string): string => {
-	const date = inputDate ? new Date(inputDate) : new Date();
-
+const getFormattedDate = (date: string): string => {
 	return format(date, WHEEL_DATE_FORMAT);
 };
 
