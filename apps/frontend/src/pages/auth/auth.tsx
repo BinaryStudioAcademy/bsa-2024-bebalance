f065b99184420621293af9f99d466229ba55fabf
iqbalalayubbi <iqbal.is415@gmail.com>
2024-09-25 15:50:16 +0700

    feat: ui improvement on the forgot pass and reset pass bb-422

--- apps/frontend/src/pages/auth/auth.tsx
+++ apps/frontend/src/pages/auth/auth.tsx
@@ -136,15 +136,10 @@ const Auth: React.FC = () => {
 								your password.
 							</div>
 						)}
-						{pathname === AppRoute.SIGN_IN ? (
+						{pathname === AppRoute.SIGN_IN && (
 							<div>
 								<span>No account? Go to </span>
 								<Link to={AppRoute.SIGN_UP}>Create an account</Link>
-							</div>
-						) : (
-							<div>
-								<span>Already have an account? Go to </span>
-								<Link to={AppRoute.SIGN_IN}>Sign In</Link>
 							</div>
 						)}
 					</span>
