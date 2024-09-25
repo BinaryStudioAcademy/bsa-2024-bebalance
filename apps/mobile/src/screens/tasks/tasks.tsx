7dd0eae3a5f1d46f0ff8f823884252eba94f042d
Tetiana Brezhynska <brezhynska9012@gmail.com>
2024-09-25 17:29:41 +0200

    feat(mobile): changed Tasks screen to show ExpiredTasksModal on getting expired tasks bb-446

--- apps/mobile/src/screens/tasks/tasks.tsx
+++ apps/mobile/src/screens/tasks/tasks.tsx
@@ -1,6 +1,8 @@
+import notifee from "@notifee/react-native";
 import React from "react";
 
 import {
+	ExpiredTasksModal,
 	LoaderWrapper,
 	PageSwitcher,
 	ScreenWrapper,
@@ -9,7 +11,7 @@ import {
 	Text,
 	View,
 } from "~/libs/components/components";
-import { DataStatus } from "~/libs/enums/enums";
+import { DataStatus, NumericalValue } from "~/libs/enums/enums";
 import {
 	useAppDispatch,
 	useAppSelector,
@@ -19,22 +21,34 @@ import {
 } from "~/libs/hooks/hooks";
 import { globalStyles } from "~/libs/styles/styles";
 import { type ValueOf } from "~/libs/types/types";
+import { type TaskDto } from "~/packages/tasks/tasks";
 import { type UserDto } from "~/packages/users/users";
 import { actions as taskActions } from "~/slices/task/task";
 import { actions as userActions } from "~/slices/users/users";
 
 import { TasksMode, TaskStatus, TaskTab } from "./libs/enums/enums";
+import {
+	addNotificationsOnTasksExpired,
+	getMillisecondsLeft,
+} from "./libs/helpers/helpers";
 import { styles } from "./styles";
+
+const MILLISECONDS_PER_MINUTE = 60_000;
 
 const Tasks: React.FC = () => {
 	const dispatch = useAppDispatch();
 
 	const authenticatedUser = useAppSelector(({ auth }) => auth.user);
-	const { dataStatus, tasks } = useAppSelector(({ tasks }) => tasks);
+	const { activeTasks, dataStatus, expiredTasks, tasks } = useAppSelector(
+		({ tasks }) => tasks,
+	);
 
 	const [mode, setMode] = useState<ValueOf<typeof TasksMode>>(
 		TasksMode.CURRENT,
 	);
+
+	const hasExpiredTasks = expiredTasks.length > NumericalValue.ZERO;
+	const taskbarTasks = mode === TasksMode.CURRENT ? activeTasks : tasks;
 
 	useEffect(() => {
 		void dispatch(
@@ -47,6 +61,14 @@ const Tasks: React.FC = () => {
 	}, [dispatch]);
 
 	useEffect(() => {
+		void addNotificationsOnTasksExpired(activeTasks);
+
+		return (): void => {
+			void notifee.cancelAllNotifications();
+		};
+	}, [activeTasks]);
+
+	useEffect(() => {
 		if (mode === TasksMode.CURRENT) {
 			void dispatch(taskActions.getCurrentTasks());
 		}
@@ -55,6 +77,29 @@ const Tasks: React.FC = () => {
 			void dispatch(taskActions.getPastTasks());
 		}
 	}, [dispatch, mode]);
+
+	useEffect(() => {
+		const currentTime = Date.now();
+		const expired: TaskDto[] = [];
+		const active: TaskDto[] = [];
+
+		for (const task of tasks) {
+			const { dueDate, status } = task;
+			const timeToDeadline = getMillisecondsLeft(currentTime, dueDate);
+
+			if (
+				timeToDeadline < MILLISECONDS_PER_MINUTE &&
+				status === TaskStatus.CURRENT
+			) {
+				expired.push(task);
+			} else {
+				active.push(task);
+			}
+		}
+
+		void dispatch(taskActions.setActiveTasks(active));
+		void dispatch(taskActions.setExpiredTasks(expired));
+	}, [tasks, dispatch]);
 
 	const handleModeToggle = useCallback(() => {
 		setMode((previousState) => {
@@ -80,40 +125,55 @@ const Tasks: React.FC = () => {
 		[dispatch],
 	);
 
+	const handleExpired = useCallback(
+		(expiredTask: TaskDto) => {
+			void dispatch(taskActions.addExpiredTask(expiredTask));
+		},
+		[dispatch],
+	);
+
 	return (
 		<ScreenWrapper edges={["top", "left", "right"]} style={styles.container}>
 			<LoaderWrapper isLoading={dataStatus === DataStatus.PENDING}>
-				<View
-					style={[
-						globalStyles.flexDirectionRow,
-						globalStyles.alignItemsCenter,
-						globalStyles.justifyContentSpaceBetween,
-						globalStyles.ph16,
-						globalStyles.pb8,
-					]}
-				>
-					<Text preset="subheading" weight="bold">
-						My Tasks
-					</Text>
-					<PageSwitcher
-						activeTab={
-							mode === TasksMode.CURRENT ? TaskTab.ACTIVE : TaskTab.PAST
-						}
-						onTabChange={handleModeToggle}
-						style={[globalStyles.flex1, styles.switch]}
-						tabs={[TaskTab.ACTIVE, TaskTab.PAST]}
-					/>
-				</View>
-				<ScrollView>
-					{tasks.map((task) => (
-						<TaskCard
-							key={task.id}
-							onComplete={handleComplete}
-							onSkip={handleSkip}
-							task={task}
-						/>
-					))}
-				</ScrollView>
+				<ExpiredTasksModal tasks={expiredTasks} />
+				{!hasExpiredTasks && (
+					<>
+						<View
+							style={[
+								globalStyles.flexDirectionRow,
+								globalStyles.alignItemsCenter,
+								globalStyles.justifyContentSpaceBetween,
+								globalStyles.ph16,
+								globalStyles.pb8,
+							]}
+						>
+							<Text preset="subheading" weight="bold">
+								My Tasks
+							</Text>
+							<PageSwitcher
+								activeTab={
+									mode === TasksMode.CURRENT ? TaskTab.ACTIVE : TaskTab.PAST
+								}
+								onTabChange={handleModeToggle}
+								style={[globalStyles.flex1, styles.switch]}
+								tabs={[TaskTab.ACTIVE, TaskTab.PAST]}
+							/>
+						</View>
+						<ScrollView
+							contentContainerStyle={[globalStyles.gap8, globalStyles.p16]}
+						>
+							{taskbarTasks.map((task) => (
+								<TaskCard
+									key={task.id}
+									onComplete={handleComplete}
+									onExpire={handleExpired}
+									onSkip={handleSkip}
+									task={task}
+								/>
+							))}
+						</ScrollView>
+					</>
+				)}
 			</LoaderWrapper>
 		</ScreenWrapper>
 	);
