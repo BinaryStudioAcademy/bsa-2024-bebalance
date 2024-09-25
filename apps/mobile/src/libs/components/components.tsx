4dd538e5f56049de9678bdcf0790dbd9b2653e3b
Tetiana Brezhynska <brezhynska9012@gmail.com>
2024-09-25 17:27:26 +0200

    feat(mobile): exported  ExpiredTaskModal bb-446

--- apps/mobile/src/libs/components/components.tsx
+++ apps/mobile/src/libs/components/components.tsx
@@ -4,6 +4,7 @@ export { Button } from "./button/button";
 export { Checkbox } from "./checkbox/checkbox";
 export { CheckboxCategoriesForm } from "./checkbox-categories-form/checkbox-categories-form";
 export { DeadlineCountdown } from "./deadline-countdown/deadline-countdown";
+export { ExpiredTasksModal } from "./expired-tasks-modal/expired-tasks-modal";
 export { GradientSlider } from "./gradient-slider/gradient-slider";
 export { GradientTabIcon } from "./gradient-tab-icon/gradient-tab-icon";
 export { GradientText } from "./gradient-text/gradient-text";
