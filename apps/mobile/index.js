f0ccd76e591aaf1bfeefc85a8f29af2b6028861a
Tetiana Brezhynska <brezhynska9012@gmail.com>
2024-09-25 17:31:40 +0200

    feat(mobile): added background event handler for app notification bb-446

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -1,6 +1,12 @@
+import notifee from "@notifee/react-native";
 import { AppRegistry } from "react-native";
 
 import { name as appName } from "./app.json";
 import { App } from "./src/libs/components/components";
+
+notifee.onBackgroundEvent(async ({ detail }) => {
+	const { notification } = detail;
+	await notifee.cancelNotification(notification.id);
+});
 
 AppRegistry.registerComponent(appName, () => App);
