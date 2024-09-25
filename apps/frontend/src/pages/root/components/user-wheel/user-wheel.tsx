b3f10687aa1afa561147589297bcf15f6e9c6513
Diana Vashchilina <vohnenna@gmail.com>
2024-09-25 13:07:17 +0300

    fix(frontend/shared): add date on the "My wheel results" (root) page bb-494 (#572)
    * feat(frontend): add scoresLastUpdatedAt to quiz slice bb-494
    
    * feat(frontend): add formatted date to UserWheel bb-494
    
    * feat(frontend): add date-related styles bb-494
    
    * feat(backend/shared): add last updatedAt score to findUserScores, updateUserScores bb-494
    
    * fix(backend/shared): use separate QuizScoresUpdateResponseDto for update bb-494
    
    * docs(backend): add updatedAt for scores bb-494
    
    * fix(frontend): use updatedAt from server response instead of slice action bb-494
    
    * fix(frontend): add const NO_SCORES_COUNT back bb-494
    
    * refactor(backend): rename getLastUpdatedScore to getLatestScoreUpdatedAt bb-494
    
    * refactor(frontend): rename get-local-datestring file to contain helper word bb-494
    
    * refactor(frontend): use shared getFormattedDate helper bb-494
    
    * refactor(backend): include orderBy logic to db query fro updatedAt bb-494
    
    * refactor(shared): include getLocalDatestring helper in shared bb-494
    
    * refactor(mobile/shared): update getFormattedDate argument bb-494
    
    * feat(frontend/mobile/shared): add dateFormat as param to getFormattedDate bb-494
    
    * refactor(frontend/mobile/shared): refactor getFormattedDate helper bb-494
    
    * refactor(backend): use SortOrder in CategoryRepository bb-494
    
    * fix(frontend/shared): update getLocalDatestring to getLocalDate helper bb-494
    
    * fix(frontend): update date container class styles bb-494
    
    * fix(frontend/shared): remove needless getLocalDate bb-494

--- apps/frontend/src/pages/root/components/user-wheel/user-wheel.tsx
+++ apps/frontend/src/pages/root/components/user-wheel/user-wheel.tsx
@@ -18,10 +18,7 @@ import {
 	RetakeQuizModal,
 	ScoresEditModal,
 } from "./libs/components/components.js";
-import {
-	getFormattedDate,
-	getLocalDatestring,
-} from "./libs/helpers/helpers.js";
+import { getFormattedDate } from "./libs/helpers/helpers.js";
 import { type WheelEditMode } from "./libs/types/types.js";
 import styles from "./styles.module.css";
 
@@ -53,10 +50,7 @@ const UserWheel: React.FC = () => {
 		: "My wheel results";
 
 	const lastWheelUpdateDate = scoresLastUpdatedAt
-		? getFormattedDate(
-				new Date(getLocalDatestring(scoresLastUpdatedAt)),
-				"d MMM yyyy, EEEE",
-			)
+		? getFormattedDate(new Date(scoresLastUpdatedAt), "d MMM yyyy, EEEE")
 		: null;
 
 	const handleEditing = useCallback(() => {
@@ -106,7 +100,7 @@ const UserWheel: React.FC = () => {
 			<div className={styles["header"]}>
 				<div className={styles["header-text-container"]}>
 					<h4 className={styles["header-text"]}>{headerText}</h4>
-					<div className={styles["date-conrainer"]}>
+					<div className={styles["date-container"]}>
 						<p className={styles["date"]}>{lastWheelUpdateDate}</p>
 					</div>
 				</div>
